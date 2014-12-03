419b055446c89e209af35ae787cdcda4804a0dbc
Julien Viet <julien@julienviet.com>
2014-12-03 15:04:15 +0100

    Use vertx-ext-parent pom for codegen and docgen

---
+++ src/main/java/io/vertx/ext/apex/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * = Vert.x Apex
+ *
+ * The {@link io.vertx.ext.apex.core.Router router} is really cool stuff, here is an example
+ *
+ * [source,{lang}]
+ * ----
+ * {@link examples.Examples#router(io.vertx.core.Vertx)}
+ * ----
+ */
+@Document(fileName = "index.ad")
+package io.vertx.ext.apex;
+
+import io.vertx.docgen.Document;
