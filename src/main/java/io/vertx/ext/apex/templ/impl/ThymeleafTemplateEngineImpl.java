66aaaa3c77c77543d0397aab675f595231b25a1e
remi128 <mremme@braintags.de>
2015-03-05 17:01:11 +0100

    added two methods to access the cache handling of the underlaying engine

--- src/main/java/io/vertx/ext/apex/templ/impl/ThymeleafTemplateEngineImpl.java
+++ src/main/java/io/vertx/ext/apex/templ/impl/ThymeleafTemplateEngineImpl.java
@@ -1,26 +1,31 @@
 /*
- * Copyright 2014 Red Hat, Inc.
- *
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  and Apache License v2.0 which accompanies this distribution.
- *
- *  The Eclipse Public License is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- *
- *  The Apache License v2.0 is available at
- *  http://www.opensource.org/licenses/apache2.0.php
- *
- *  You may elect to redistribute this code under either of these licenses.
+ * Copyright 2014 Red Hat, Inc. All rights reserved. This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0 and Apache License v2.0 which accompanies this distribution. The
+ * Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html The Apache License v2.0 is available
+ * at http://www.opensource.org/licenses/apache2.0.php You may elect to redistribute this code under either of these
+ * licenses.
  */
 
 package io.vertx.ext.apex.templ.impl;
 
-import io.vertx.core.*;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxException;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.apex.RoutingContext;
 import io.vertx.ext.apex.impl.Utils;
 import io.vertx.ext.apex.templ.ThymeleafTemplateEngine;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.Locale;
+
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.TemplateProcessingParameters;
 import org.thymeleaf.context.IContext;
@@ -28,16 +33,13 @@ import org.thymeleaf.context.VariablesMap;
 import org.thymeleaf.resourceresolver.IResourceResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
-import java.io.*;
-import java.util.Locale;
-
 /**
  * @author <a href="http://pmlopes@gmail.com">Paulo Lopes</a>
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class ThymeleafTemplateEngineImpl implements ThymeleafTemplateEngine {
 
-  private final TemplateEngine engine = new TemplateEngine();
+  private final TemplateEngine   engine   = new TemplateEngine();
   private final TemplateResolver templateResolver;
   private final ResourceResolver resolver = new ResourceResolver();
 
@@ -87,24 +89,32 @@ public class ThymeleafTemplateEngineImpl implements ThymeleafTemplateEngine {
     }
   }
 
-  /**
-   * Return the underlaying Thymeleaf engine for further configuration
-   * 
-   * @return
-   */
-  public TemplateEngine getEngine(){
-	return engine;  
-  }
-  
   /*
-   We extend VariablesMap to avoid copying all context map data for each render
-   We put the context data Map directly into the variable map and we also provide
-   variables called:
-   _context - this is the routing context itself
-   _request - this is the HttpServerRequest object
-   _response - this is the HttpServerResponse object
-    */
-  private static class ApexIContext implements IContext  {
+   * (non-Javadoc)
+   * @see io.vertx.ext.apex.templ.ThymeleafTemplateEngine#setCacheable(boolean)
+   */
+  @Override
+  public ThymeleafTemplateEngine setCacheable(boolean cacheable) {
+    templateResolver.setCacheable(cacheable);
+    return this;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see io.vertx.ext.apex.templ.ThymeleafTemplateEngine#setCacheTTLMs(java.lang.Long)
+   */
+  @Override
+  public ThymeleafTemplateEngine setCacheTTLMs(Long cacheTTLMs) {
+    templateResolver.setCacheTTLMs(cacheTTLMs);
+    return this;
+  }
+
+  /*
+   * We extend VariablesMap to avoid copying all context map data for each render We put the context data Map directly
+   * into the variable map and we also provide variables called: _context - this is the routing context itself _request
+   * - this is the HttpServerRequest object _response - this is the HttpServerResponse object
+   */
+  private static class ApexIContext implements IContext {
 
     private final VariablesMap<String, Object> data;
 
@@ -142,7 +152,8 @@ public class ThymeleafTemplateEngineImpl implements ThymeleafTemplateEngine {
     }
 
     @Override
-    public InputStream getResourceAsStream(TemplateProcessingParameters templateProcessingParameters, String resourceName) {
+    public InputStream getResourceAsStream(TemplateProcessingParameters templateProcessingParameters,
+        String resourceName) {
       String str = Utils.readFileToString(vertx, resourceName);
       try {
         ByteArrayInputStream bis = new ByteArrayInputStream(str.getBytes("UTF-8"));
@@ -153,8 +164,5 @@ public class ThymeleafTemplateEngineImpl implements ThymeleafTemplateEngine {
       }
     }
   }
-
-
-
 
 }
