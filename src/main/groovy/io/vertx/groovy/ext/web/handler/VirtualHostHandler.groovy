d3907056ed3a80eabb40e8dec8b072686a525760
Paulo Lopes <paulo@mlopes.net>
2015-07-27 13:32:03 +0200

    #12 implement virtual host

---
+++ src/main/groovy/io/vertx/groovy/ext/web/handler/VirtualHostHandler.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.vertx.groovy.ext.web.handler;
+import groovy.transform.CompileStatic
+import io.vertx.lang.groovy.InternalHelper
+import io.vertx.groovy.ext.web.RoutingContext
+import io.vertx.core.Handler
+/**
+ * Handler that will filter requests based on the request Host name.
+*/
+@CompileStatic
+public class VirtualHostHandler implements Handler<RoutingContext> {
+  private final def io.vertx.ext.web.handler.VirtualHostHandler delegate;
+  public VirtualHostHandler(Object delegate) {
+    this.delegate = (io.vertx.ext.web.handler.VirtualHostHandler) delegate;
+  }
+  public Object getDelegate() {
+    return delegate;
+  }
+  public void handle(RoutingContext arg0) {
+    ((io.vertx.core.Handler) this.delegate).handle((io.vertx.ext.web.RoutingContext)arg0.getDelegate());
+  }
+  /**
+   * Create a handler
+   * @param hostname 
+   * @param handler 
+   * @return the handler
+   */
+  public static VirtualHostHandler create(String hostname, Handler<RoutingContext> handler) {
+    def ret= InternalHelper.safeCreate(io.vertx.ext.web.handler.VirtualHostHandler.create(hostname, new Handler<io.vertx.ext.web.RoutingContext>() {
+      public void handle(io.vertx.ext.web.RoutingContext event) {
+        handler.handle(new io.vertx.groovy.ext.web.RoutingContext(event));
+      }
+    }), io.vertx.groovy.ext.web.handler.VirtualHostHandler.class);
+    return ret;
+  }
+}
---
+++ src/main/java/io/vertx/ext/web/handler/impl/VirtualHostHandlerImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+package io.vertx.ext.web.handler.impl;
+
+import io.vertx.core.Handler;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.VirtualHostHandler;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="http://plopes@redhat.com">Paulo Lopes</a>
+ */
+public class VirtualHostHandlerImpl implements VirtualHostHandler {
+
+  private final Pattern regex;
+  private final Handler<RoutingContext> handler;
+
+  public VirtualHostHandlerImpl(String hostname, Handler<RoutingContext> handler) {
+    this.handler = handler;
+    this.regex = Pattern.compile("^" + hostname.replaceAll("\\.", "\\\\.").replaceAll("[*]", "(.*?)") + "$", Pattern.CASE_INSENSITIVE);
+  }
+
+  @Override
+  public void handle(RoutingContext ctx) {
+    String host = ctx.request().getHeader("host");
+    if (host == null) {
+      ctx.next();
+    } else {
+      boolean match = false;
+      for (String h : host.split(":")) {
+        if (regex.matcher(h).matches()) {
+          match = true;
+          break;
+        }
+      }
+
+      if (match) {
+        handler.handle(ctx);
+      } else {
+        ctx.next();
+      }
+    }
+  }
+}
