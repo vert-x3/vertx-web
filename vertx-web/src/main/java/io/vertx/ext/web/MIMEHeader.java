49007da4d100edc0cbf100923a84645ad7a5fbab
brunoais <brunoaiss@gmail.com>
2016-09-15 07:47:55 +0100

    MIMEHeader method documentation

--- vertx-web/src/main/java/io/vertx/ext/web/MIMEHeader.java
+++ vertx-web/src/main/java/io/vertx/ext/web/MIMEHeader.java
@@ -2,8 +2,17 @@ package io.vertx.ext.web;
 
 public interface MIMEHeader extends ParsedHeaderValue{
   
+  /**
+   * Gets the parsed component part of the MIME. This is the string between the beginning and the first '/' of the MIME
+   * @return The component of the MIME this represents
+   */
   String component();
   
+  /**
+   * Gets the parsed subcomponent part of the MIME. This is the string between the first '/' and the ';'
+   * or the end of the MIME
+   * @return The subcomponent of the MIME this represents 
+   */
   String subComponent();
   
 }
