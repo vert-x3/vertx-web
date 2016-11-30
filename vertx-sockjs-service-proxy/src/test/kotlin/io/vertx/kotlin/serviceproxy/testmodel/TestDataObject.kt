93bbb352797a9e10600725003602cc2a31036464
Paulo Lopes <paulo@mlopes.net>
2016-11-30 11:06:33 +0100

    document the PRNG functionality

--- vertx-sockjs-service-proxy/src/test/kotlin/io/vertx/kotlin/serviceproxy/testmodel/TestDataObject.kt
+++ vertx-sockjs-service-proxy/src/test/kotlin/io/vertx/kotlin/serviceproxy/testmodel/TestDataObject.kt
@@ -3,21 +3,21 @@ package io.vertx.kotlin.serviceproxy.testmodel
 import io.vertx.serviceproxy.testmodel.TestDataObject
 
 fun TestDataObject(
-        bool: Boolean? = null,
-    number: Int? = null,
-    string: String? = null): TestDataObject = io.vertx.serviceproxy.testmodel.TestDataObject().apply {
+    bool: Boolean? = null,
+  number: Int? = null,
+  string: String? = null): TestDataObject = io.vertx.serviceproxy.testmodel.TestDataObject().apply {
 
-    if (bool != null) {
-        this.isBool = bool
-    }
+  if (bool != null) {
+    this.isBool = bool
+  }
 
-    if (number != null) {
-        this.number = number
-    }
+  if (number != null) {
+    this.number = number
+  }
 
-    if (string != null) {
-        this.string = string
-    }
+  if (string != null) {
+    this.string = string
+  }
 
 }
 
