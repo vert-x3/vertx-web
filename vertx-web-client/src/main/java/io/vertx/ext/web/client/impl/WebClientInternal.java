9f92e485ae0f73c0b03a51c15d38e3f6b56fac04
Julien Viet <julien@julienviet.com>
2017-07-11 10:47:54 +0200

    Added more javadoc on addInterceptor method

--- vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/WebClientInternal.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/WebClientInternal.java
@@ -24,8 +24,13 @@ import io.vertx.ext.web.client.WebClient;
 public interface WebClientInternal extends WebClient {
 
   /**
-   * Add interceptor.
-   * <p>
+   * Add interceptor in the chain.
+   * <p/>
+   * The interceptor can maintain per request state with {@link HttpContext#get(String)}/{@link HttpContext#set(String, Object)}.
+   * <p/>
+   * A request/response can be processed several times (in case of retry) and thus they should use the per request state
+   * to ensure an operation is not done twice.
+   * <p/>
    * This API is internal and is subject to change or to be replaced by another one.
    *
    * @param interceptor the interceptor to add
