77d704fa64996026d6bd66153210d90fde42dfce
Francesco Guardiani <francescoguard@gmail.com>
2018-03-31 17:00:33 +0200

    Fixed #874
    Signed-off-by: francesco <francescoguard@gmail.com>

--- vertx-web/src/test/java/io/vertx/ext/web/WebTestWithWebClientBase.java
+++ vertx-web/src/test/java/io/vertx/ext/web/WebTestWithWebClientBase.java
@@ -3,6 +3,7 @@ package io.vertx.ext.web;
 import io.vertx.core.MultiMap;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
@@ -66,6 +67,17 @@ public class WebTestWithWebClientBase extends WebTestBase {
       assertEquals(statusCode, ar.result().statusCode());
       assertEquals(statusMessage, ar.result().statusMessage());
       assertEquals(obj, ar.result().bodyAsJsonObject());
+      latch.countDown();
+    });
+    awaitLatch(latch);
+  }
+
+  public void testRequestWithJSONArray(HttpMethod method, String path, JsonArray jsonArray, int statusCode, String statusMessage, JsonArray arr) throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+    webClient.request(method, 8080, "localhost", path).sendJson(jsonArray, (ar) -> {
+      assertEquals(statusCode, ar.result().statusCode());
+      assertEquals(statusMessage, ar.result().statusMessage());
+      assertEquals(arr, ar.result().bodyAsJsonArray());
       latch.countDown();
     });
     awaitLatch(latch);
