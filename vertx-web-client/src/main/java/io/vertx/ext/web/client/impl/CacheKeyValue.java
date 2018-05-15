054e45717a20b9c48a88ed115d7add9dc01a2403
Alexey Soshin <alexey.soshin@gettaxi.com>
2018-05-15 18:01:51 +0200

    Extract CacheKeyValue enum to a separate file

---
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/CacheKeyValue.java
@@ -0,0 +1,14 @@
+package io.vertx.ext.web.client.impl;
+
+/**
+ * Allowed keys for Web Client cache
+ * @author Alexey Soshin
+ */
+public enum CacheKeyValue {
+    METHOD,
+    HOST,
+    PORT,
+    URI,
+    PARAMS,
+    CONTENT_TYPE
+}
