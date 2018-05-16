1104cbdb9fa95e5849fcc9f1ac224adc9843f8e4
Alexey Soshin <alexey.soshin@gettaxi.com>
2018-05-16 21:53:49 +0200

    Improve JavaDoc of CacheManager

--- vertx-web-client/src/main/java/io/vertx/ext/web/client/cache/CacheManager.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/cache/CacheManager.java
@@ -1,12 +1,12 @@
 package io.vertx.ext.web.client.cache;
 
-import io.vertx.core.Handler;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.HttpResponse;
 
 import java.util.Optional;
 
 /**
+ * Abstracts caching of HTTP requests and responses
  * @author Alexey Soshin
  */
 public interface CacheManager {
@@ -23,7 +23,16 @@ public interface CacheManager {
      */
     Optional<HttpResponse<Object>> fetch(HttpRequest request);
 
+    /**
+     * Puts new value in cache, where the key is based on request and value on response
+     * @param request   HTTP request that was issues
+     * @param response  HTTP response to be associated
+     */
     void put(HttpRequest request, HttpResponse<Object> response);
 
+    /**
+     * Removes value from cache corresponding to request key
+     * @param request HTTP request that should be removed from cache
+     */
     void remove(HttpRequest request);
 }
