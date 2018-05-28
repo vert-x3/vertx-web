7c0ed19f9656dc559071708e4d6a28f88fc40521
torahmike <mwiesenberg@gmail.com>
2018-05-28 11:41:16 -0400

    Remove unused import

--- vertx-web/src/test/java/io/vertx/ext/web/WebTestBase.java
+++ vertx-web/src/test/java/io/vertx/ext/web/WebTestBase.java
@@ -30,7 +30,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.HashSet;
