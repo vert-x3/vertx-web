a9a0ab1a9bf4b1eb1137832da40e22eea1c3cfe4
Paulo Lopes <paulo@mlopes.net>
2018-08-08 13:40:15 +0200

    Added javadocs

--- vertx-web-session-stores/vertx-web-sstore-cookie/src/main/java/io/vertx/ext/web/sstore/cookie/CookieSessionStore.java
+++ vertx-web-session-stores/vertx-web-sstore-cookie/src/main/java/io/vertx/ext/web/sstore/cookie/CookieSessionStore.java
@@ -15,29 +15,34 @@
  */
 package io.vertx.ext.web.sstore.cookie;
 
-import io.vertx.codegen.annotations.Nullable;
 import io.vertx.codegen.annotations.VertxGen;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.PRNG;
-import io.vertx.ext.web.Session;
 import io.vertx.ext.web.sstore.SessionStore;
 import io.vertx.ext.web.sstore.cookie.impl.CookieSessionStoreImpl;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 /**
+ * A SessionStore that uses a Cookie to store the session data. All data is stored in
+ * plain sight and signed using a HMAC using the given secret.
+ *
+ * The signature ensures that the cookie payload is not tampered when returning from
+ * the user agent (browser) back to the server.
+ *
  * @author <a href="mailto:plopes@redhat.com">Paulo Lopes</a>
  */
 @VertxGen
 public interface CookieSessionStore extends SessionStore {
 
+  /**
+   * Creates a CookieSessionStore.
+   *
+   * Cookie data will be signed using the given secret. The secret as the name
+   * reflects, should never leave the server, otherwise user agents could tamper
+   * with the payload.
+   *
+   * @param vertx a vert.x instance
+   * @param secret a secret to feed the HMAC algorithm
+   * @return the store
+   */
   static CookieSessionStore create(Vertx vertx, String secret) {
     return new CookieSessionStoreImpl(vertx, secret);
   }
