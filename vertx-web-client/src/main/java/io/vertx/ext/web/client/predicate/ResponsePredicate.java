9407e580c221eebc89569a76b9d20f121e583306
Thomas Segismont <tsegismo@redhat.com>
2018-09-26 18:42:34 +0200

    Added all status codes predicates

--- vertx-web-client/src/main/java/io/vertx/ext/web/client/predicate/ResponsePredicate.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/predicate/ResponsePredicate.java
@@ -28,12 +28,319 @@ import java.util.function.Function;
 @VertxGen
 public interface ResponsePredicate {
 
+  /**
+   * Any 1XX informational response
+   */
+  ResponsePredicate SC_INFORMATIONAL_RESPONSE = status(100, 200);
+
+  /**
+   * 100 Continue
+   */
+  ResponsePredicate SC_CONTINUE = status(100);
+
+  /**
+   * 101 Switching Protocols
+   */
+  ResponsePredicate SC_SWITCHING_PROTOCOLS = status(101);
+
+  /**
+   * 102 Processing (WebDAV, RFC2518)
+   */
+  ResponsePredicate SC_PROCESSING = status(102);
+
+  /**
+   * 103 Early Hints
+   */
+  ResponsePredicate SC_EARLY_HINTS = status(103);
+
+  /**
+   * Any 2XX success
+   */
   ResponsePredicate SC_SUCCESS = status(200, 300);
+
+  /**
+   * 200 OK
+   */
+  ResponsePredicate SC_OK = status(200);
+
+  /**
+   * 201 Created
+   */
+  ResponsePredicate SC_CREATED = status(201);
+
+  /**
+   * 202 Accepted
+   */
+  ResponsePredicate SC_ACCEPTED = status(202);
+
+  /**
+   * 203 Non-Authoritative Information (since HTTP/1.1)
+   */
+  ResponsePredicate SC_NON_AUTHORITATIVE_INFORMATION = status(203);
+
+  /**
+   * 204 No Content
+   */
+  ResponsePredicate SC_NO_CONTENT = status(204);
+
+  /**
+   * 205 Reset Content
+   */
+  ResponsePredicate SC_RESET_CONTENT = status(205);
+
+  /**
+   * 206 Partial Content
+   */
+  ResponsePredicate SC_PARTIAL_CONTENT = status(206);
+
+  /**
+   * 207 Multi-Status (WebDAV, RFC2518)
+   */
+  ResponsePredicate SC_MULTI_STATUS = status(207);
+
+  /**
+   * Any 3XX redirection
+   */
+  ResponsePredicate SC_REDIRECTION = status(300, 400);
+
+  /**
+   * 300 Multiple Choices
+   */
+  ResponsePredicate SC_MULTIPLE_CHOICES = status(300);
+
+  /**
+   * 301 Moved Permanently
+   */
+  ResponsePredicate SC_MOVED_PERMANENTLY = status(301);
+
+  /**
+   * 302 Found
+   */
+  ResponsePredicate SC_FOUND = status(302);
+
+  /**
+   * 303 See Other (since HTTP/1.1)
+   */
+  ResponsePredicate SC_SEE_OTHER = status(303);
+
+  /**
+   * 304 Not Modified
+   */
+  ResponsePredicate SC_NOT_MODIFIED = status(304);
+
+  /**
+   * 305 Use Proxy (since HTTP/1.1)
+   */
+  ResponsePredicate SC_USE_PROXY = status(305);
+
+  /**
+   * 307 Temporary Redirect (since HTTP/1.1)
+   */
+  ResponsePredicate SC_TEMPORARY_REDIRECT = status(307);
+
+  /**
+   * 308 Permanent Redirect (RFC7538)
+   */
+  ResponsePredicate SC_PERMANENT_REDIRECT = status(308);
+
+  /**
+   * Any 4XX client error
+   */
+  ResponsePredicate SC_CLIENT_ERRORS = status(400, 500);
+
+  /**
+   * 400 Bad Request
+   */
+  ResponsePredicate SC_BAD_REQUEST = status(400);
+
+  /**
+   * 401 Unauthorized
+   */
+  ResponsePredicate SC_UNAUTHORIZED = status(401);
+
+  /**
+   * 402 Payment Required
+   */
+  ResponsePredicate SC_PAYMENT_REQUIRED = status(402);
+
+  /**
+   * 403 Forbidden
+   */
+  ResponsePredicate SC_FORBIDDEN = status(403);
+
+  /**
+   * 404 Not Found
+   */
+  ResponsePredicate SC_NOT_FOUND = status(404);
+
+  /**
+   * 405 Method Not Allowed
+   */
+  ResponsePredicate SC_METHOD_NOT_ALLOWED = status(405);
+
+  /**
+   * 406 Not Acceptable
+   */
+  ResponsePredicate SC_NOT_ACCEPTABLE = status(406);
+
+  /**
+   * 407 Proxy Authentication Required
+   */
+  ResponsePredicate SC_PROXY_AUTHENTICATION_REQUIRED = status(407);
+
+  /**
+   * 408 Request Timeout
+   */
+  ResponsePredicate SC_REQUEST_TIMEOUT = status(408);
+
+  /**
+   * 409 Conflict
+   */
+  ResponsePredicate SC_CONFLICT = status(409);
+
+  /**
+   * 410 Gone
+   */
+  ResponsePredicate SC_GONE = status(410);
+
+  /**
+   * 411 Length Required
+   */
+  ResponsePredicate SC_LENGTH_REQUIRED = status(411);
+
+  /**
+   * 412 Precondition Failed
+   */
+  ResponsePredicate SC_PRECONDITION_FAILED = status(412);
+
+  /**
+   * 413 Request Entity Too Large
+   */
+  ResponsePredicate SC_REQUEST_ENTITY_TOO_LARGE = status(413);
+
+  /**
+   * 414 Request-URI Too Long
+   */
+  ResponsePredicate SC_REQUEST_URI_TOO_LONG = status(414);
+
+  /**
+   * 415 Unsupported Media Type
+   */
+  ResponsePredicate SC_UNSUPPORTED_MEDIA_TYPE = status(415);
+
+  /**
+   * 416 Requested Range Not Satisfiable
+   */
+  ResponsePredicate SC_REQUESTED_RANGE_NOT_SATISFIABLE = status(416);
+
+  /**
+   * 417 Expectation Failed
+   */
+  ResponsePredicate SC_EXPECTATION_FAILED = status(417);
+
+  /**
+   * 421 Misdirected Request
+   */
+  ResponsePredicate SC_MISDIRECTED_REQUEST = status(421);
+
+  /**
+   * 422 Unprocessable Entity (WebDAV, RFC4918)
+   */
+  ResponsePredicate SC_UNPROCESSABLE_ENTITY = status(422);
+
+  /**
+   * 423 Locked (WebDAV, RFC4918)
+   */
+  ResponsePredicate SC_LOCKED = status(423);
+
+  /**
+   * 424 Failed Dependency (WebDAV, RFC4918)
+   */
+  ResponsePredicate SC_FAILED_DEPENDENCY = status(424);
+
+  /**
+   * 425 Unordered Collection (WebDAV, RFC3648)
+   */
+  ResponsePredicate SC_UNORDERED_COLLECTION = status(425);
+
+  /**
+   * 426 Upgrade Required (RFC2817)
+   */
+  ResponsePredicate SC_UPGRADE_REQUIRED = status(426);
+
+  /**
+   * 428 Precondition Required (RFC6585)
+   */
+  ResponsePredicate SC_PRECONDITION_REQUIRED = status(428);
+
+  /**
+   * 429 Too Many Requests (RFC6585)
+   */
+  ResponsePredicate SC_TOO_MANY_REQUESTS = status(429);
+
+  /**
+   * 431 Request Header Fields Too Large (RFC6585)
+   */
+  ResponsePredicate SC_REQUEST_HEADER_FIELDS_TOO_LARGE = status(431);
+
+  /**
+   * Any 5XX server error
+   */
+  ResponsePredicate SC_SERVER_ERRORS = status(500, 600);
+
+  /**
+   * 500 Internal Server Error
+   */
+  ResponsePredicate SC_INTERNAL_SERVER_ERROR = status(500);
+
+  /**
+   * 501 Not Implemented
+   */
+  ResponsePredicate SC_NOT_IMPLEMENTED = status(501);
+
+  /**
+   * 502 Bad Gateway
+   */
+  ResponsePredicate SC_BAD_GATEWAY = status(502);
+
+  /**
+   * 503 Service Unavailable
+   */
+  ResponsePredicate SC_SERVICE_UNAVAILABLE = status(503);
+
+  /**
+   * 504 Gateway Timeout
+   */
+  ResponsePredicate SC_GATEWAY_TIMEOUT = status(504);
+
+  /**
+   * 505 HTTP Version Not Supported
+   */
+  ResponsePredicate SC_HTTP_VERSION_NOT_SUPPORTED = status(505);
+
+  /**
+   * 506 Variant Also Negotiates (RFC2295)
+   */
+  ResponsePredicate SC_VARIANT_ALSO_NEGOTIATES = status(506);
+
+  /**
+   * 507 Insufficient Storage (WebDAV, RFC4918)
+   */
+  ResponsePredicate SC_INSUFFICIENT_STORAGE = status(507);
+
+  /**
+   * 510 Not Extended (RFC2774)
+   */
+  ResponsePredicate SC_NOT_EXTENDED = status(510);
+
+  /**
+   * 511 Network Authentication Required (RFC6585)
+   */
+  ResponsePredicate SC_NETWORK_AUTHENTICATION_REQUIRED = status(511);
 
   static ResponsePredicate status(int statusCode) {
     return status(statusCode, statusCode + 1);
   }
-
 
   /**
    * Return a predicate asserting that the status response code is in the {@code [min,max[} range.
