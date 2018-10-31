d57042ab3aaa55970b83d0ea55abff5c940c9499
tnolli <tommaso.nolli@gmail.com>
2018-10-31 16:11:46 +0100

    Reworked code to use the new interceptors mechanism

--- vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
@@ -5,26 +5,44 @@ import java.util.List;
 import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
 import io.netty.handler.codec.http.cookie.ClientCookieEncoder;
 import io.netty.handler.codec.http.cookie.Cookie;
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.CaseInsensitiveHeaders;
 import io.vertx.ext.web.client.CookieStore;
-import io.vertx.ext.web.client.HttpResponse;
 
-public class SessionAwareInterceptor implements Handler<HttpContext> {
+public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
   public static final String CLIENT_CONTEXT_KEY = "_SessionAwareWebClient";
   private static final String HEADERS_CONTEXT_KEY = "_originalHeaders";
   
   @Override
-  public void handle(HttpContext context) {
+  public void handle(HttpContext<?> context) {
+    switch(context.phase()) {
+    case PREPARE_REQUEST:
+      prepareRequest(context);
+      break;
+    case DISPATCH_RESPONSE:
+      processResponse(context);
+    default:
+      break;
+    }
+    
+    context.next();
+  }
+
+  private void prepareRequest(HttpContext<?> context) {
+    
     HttpRequestImpl<?> request = (HttpRequestImpl<?>) context.request();
-    SessionAwareWebClientImpl webclient = context.get(CLIENT_CONTEXT_KEY);
+    SessionAwareWebClientImpl webclient = (SessionAwareWebClientImpl) context.get(CLIENT_CONTEXT_KEY);
     assert webclient != null : "WRONG API usage: missing SessionAwareWebClient in HttpContext";
 
+    MultiMap headers = context.get(HEADERS_CONTEXT_KEY);
+    if (headers == null) {
+      headers = new CaseInsensitiveHeaders().addAll(request.headers());
+      context.set(SessionAwareInterceptor.HEADERS_CONTEXT_KEY, headers);
+    }
+    
     // we need to reset the headers at every "send" because cookies can be changed,
     // either by the server (that sent new ones) or by the user.
-    MultiMap headers = context.get(HEADERS_CONTEXT_KEY);
     request.headers().clear().addAll(headers).addAll(webclient.headers());
 
     String domain = request.virtualHost;
@@ -42,45 +60,39 @@ public class SessionAwareInterceptor implements Handler<HttpContext> {
     for (Cookie c : cookies) {
       request.headers().add("cookie", ClientCookieEncoder.STRICT.encode(c));
     }
-    
-    Handler<AsyncResult<HttpResponse<Object>>> originalHandler = context.getResponseHandler();
-    context.setResponseHandler(ar -> {
-      if (ar.succeeded()) {
-        List<String> cookieHeaders = ar.result().cookies();
-        if (cookieHeaders == null) {
-          originalHandler.handle(ar);
-          return;
-        }
-        CookieStore cookieStore = webclient.getCookieStore();
-        cookieHeaders.forEach(header -> {
-          Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
-          if (cookie != null) {
-            if (cookie.domain() == null) {
-              // Set the domain if missing, because we need to send cookies
-              // only to the domains we received them from.
-              cookie.setDomain(request.virtualHost != null ? request.virtualHost : request.host);
-            }
-            if (cookieStore instanceof InternalCookieStore) {
-              ((InternalCookieStore) cookieStore).put(cookie);
-            } else {
-              cookieStore.put(cookie.name(), cookie.value(), cookie.domain(), cookie.path(), cookie.maxAge(),
-                  cookie.isSecure());
-            }
-          }
-        });
-      }
-      originalHandler.handle(ar);
-    });
-    
-    context.next();
   }
 
-  public static void prepareContext(HttpContext context, SessionAwareWebClientImpl client) {
-    context.set(SessionAwareInterceptor.CLIENT_CONTEXT_KEY, client);
-
+  private void processResponse(HttpContext<?> context) {
+    List<String> cookieHeaders = context.response().cookies();
+    if (cookieHeaders == null) {
+      return;
+    }
+    
+    SessionAwareWebClientImpl webclient = (SessionAwareWebClientImpl) context.get(CLIENT_CONTEXT_KEY);
+    assert webclient != null : "WRONG API usage: missing SessionAwareWebClient in HttpContext";
+    
     HttpRequestImpl<?> request = (HttpRequestImpl<?>) context.request();
-    MultiMap headers = new CaseInsensitiveHeaders().addAll(request.headers());
-    context.set(SessionAwareInterceptor.HEADERS_CONTEXT_KEY, headers);
+    CookieStore cookieStore = webclient.getCookieStore();
+    cookieHeaders.forEach(header -> {
+      Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
+      if (cookie != null) {
+        if (cookie.domain() == null) {
+          // Set the domain if missing, because we need to send cookies
+          // only to the domains we received them from.
+          cookie.setDomain(request.virtualHost != null ? request.virtualHost : request.host);
+        }
+        if (cookieStore instanceof InternalCookieStore) {
+          ((InternalCookieStore) cookieStore).put(cookie);
+        } else {
+          cookieStore.put(cookie.name(), cookie.value(), cookie.domain(), cookie.path(), cookie.maxAge(),
+              cookie.isSecure());
+        }
+      }
+    });
+  }
+
+  public static void prepareContext(HttpContext<?> context, SessionAwareWebClientImpl client) {
+    context.set(SessionAwareInterceptor.CLIENT_CONTEXT_KEY, client);
   }
 
 }
