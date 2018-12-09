2837f254f49d1f11aa8f9c649ee1aa7ecd27e2e5
Francesco Guardiani <francescoguard@gmail.com>
2018-12-09 18:57:04 +0100

    Porting fix #861 (#1100)
    Signed-off-by: francesco <francescoguard@gmail.com>

--- vertx-web-api-contract/src/test/java/io/vertx/ext/web/api/contract/openapi3/OpenAPI3RouterFactoryTest.java
+++ vertx-web-api-contract/src/test/java/io/vertx/ext/web/api/contract/openapi3/OpenAPI3RouterFactoryTest.java
@@ -11,6 +11,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.ApiWebTestBase;
+import io.vertx.ext.web.api.RequestParameter;
 import io.vertx.ext.web.api.RequestParameters;
 import io.vertx.ext.web.api.contract.RouterFactoryException;
 import io.vertx.ext.web.api.contract.RouterFactoryOptions;
@@ -55,7 +56,7 @@ public class OpenAPI3RouterFactoryTest extends ApiWebTestBase {
   }
 
   private void startServer() throws InterruptedException {
-    Router router = routerFactory.getRouter();
+    router = routerFactory.getRouter();
     server = vertx.createHttpServer(new HttpServerOptions().setPort(8080).setHost("localhost"));
     CountDownLatch latch = new CountDownLatch(1);
     server.requestHandler(router).listen(onSuccess(res -> latch.countDown()));
@@ -889,5 +890,39 @@ public class OpenAPI3RouterFactoryTest extends ApiWebTestBase {
     } finally {
       Paths.get("./my-uploads").toFile().deleteOnExit();
     }
+  }
+
+  @Test
+  public void testSharedRequestBody() throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+    OpenAPI3RouterFactory.create(this.vertx, "src/test/resources/swaggers/shared_request_body.yaml",
+      openAPI3RouterFactoryAsyncResult -> {
+        routerFactory = openAPI3RouterFactoryAsyncResult.result();
+        routerFactory.setOptions(HANDLERS_TESTS_OPTIONS);
+
+        final Handler<RoutingContext> handler = routingContext -> {
+          RequestParameters params = routingContext.get("parsedParameters");
+          RequestParameter body = params.body();
+          JsonObject jsonBody = body.getJsonObject();
+          routingContext
+            .response()
+            .setStatusCode(200)
+            .setStatusMessage("OK")
+            .putHeader("Content-Type", "application/json")
+            .end(jsonBody.toBuffer());
+        };
+
+        routerFactory.addHandlerByOperationId("thisWayWorks", handler);
+        routerFactory.addHandlerByOperationId("thisWayBroken", handler);
+
+        latch.countDown();
+      });
+    awaitLatch(latch);
+
+    startServer();
+
+    JsonObject obj = new JsonObject().put("id", "aaa").put("name", "bla");
+    testRequestWithJSON(HttpMethod.POST, "/v1/working", obj.toBuffer(), 200, "OK", obj.toBuffer());
+    testRequestWithJSON(HttpMethod.POST, "/v1/notworking", obj.toBuffer(), 200, "OK", obj.toBuffer());
   }
 }
