9ab72c1068bb255a75e73c85030899fb8adf3607
Thomas Segismont <tsegismo@redhat.com>
2019-01-25 16:30:33 +0100

    Minor SockJS tests refactoring
    The new SockJSTestBase creates a real user environment:
    two verticle instances, two routers, two handlers, ... etc
    
    Still sharing the SockJS session at runtime

--- vertx-web/src/test/java/io/vertx/ext/web/handler/sockjs/SockJSAsyncHandlerTest.java
+++ vertx-web/src/test/java/io/vertx/ext/web/handler/sockjs/SockJSAsyncHandlerTest.java
@@ -1,30 +1,53 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.vertx.ext.web.handler.sockjs;
 
+import io.vertx.ext.web.handler.BodyHandler;
 import org.junit.Test;
 
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
-
+/**
+ * @author Ben Ripkens
+ */
 public class SockJSAsyncHandlerTest extends SockJSTestBase {
 
-  public SockJSAsyncHandlerTest() {
+  @Override
+  public void setUp() throws Exception {
     // Use two servers so we test with HTTP request/response with load balanced SockJSSession access
     numServers = 2;
-  }
-
-  @Override
-  protected void addHandlersBeforeSockJSHandler(Router router) {
-    router.route().handler(BodyHandler.create());
-    // simulate an async handler
-    router.route().handler(rtx -> rtx.vertx().executeBlocking(f -> f.complete(true), r -> rtx.next()));
+    preSockJSHandlerSetup = router -> {
+      router.route().handler(BodyHandler.create());
+      // simulate an async handler
+      router.route().handler(rtx -> rtx.vertx().executeBlocking(f -> f.complete(true), r -> rtx.next()));
+    };
+    super.setUp();
   }
 
   @Test
-  public void testHandleMessageFromXhrTransportWithAsyncHandler() {
-    sockJSHandler.socketHandler(socket -> socket.handler(buf -> {
-      assertEquals("Hello World", buf.toString());
-      testComplete();
-    }));
+  public void testHandleMessageFromXhrTransportWithAsyncHandler() throws Exception {
+    socketHandler = () -> {
+      return socket -> {
+        socket.handler(buf -> {
+          assertEquals("Hello World", buf.toString());
+          testComplete();
+        });
+      };
+    };
+
+    startServers();
 
     client.post("/test/400/8ne8e94a/xhr", onSuccess(resp -> {
       assertEquals(200, resp.statusCode());
