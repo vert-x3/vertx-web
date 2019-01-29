303b4f37da950682caba60c6554fb55f24b6815f
Clement Escoffier <clement.escoffier@gmail.com>
2019-01-29 08:31:12 +0100

    Implement a JSON stream body codec
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ vertx-web-client/src/test/java/io/vertx/ext/web/client/JsonStreamTest.java
@@ -0,0 +1,107 @@
+package io.vertx.ext.web.client;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.parsetools.JsonParser;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.codec.BodyCodec;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Checks the behavior of the {@link io.vertx.ext.web.codec.impl.JsonStreamBodyCodec}.
+ */
+@RunWith(VertxUnitRunner.class)
+public class JsonStreamTest {
+
+  private Vertx vertx;
+  private WebClient client;
+
+  @Before
+  public void setup(TestContext tc) {
+    vertx = Vertx.vertx();
+    client = WebClient.create(vertx, new WebClientOptions().setDefaultPort(8080).setDefaultHost("localhost"));
+    Async async = tc.async();
+
+    vertx.createHttpServer().requestHandler(req -> {
+      int count = Integer.valueOf(req.getParam("count"));
+      String separator = req.getParam("separator");
+      if (separator.equalsIgnoreCase("nl")) {
+        separator = "\n";
+      }
+      if (separator.equalsIgnoreCase("bl")) {
+        separator = "\r\n";
+      }
+      req.response().setChunked(true);
+      for (int i = 0; i < count; i++) {
+        // Send chunks...
+        JsonObject json = new JsonObject().put("count", i).put("data", "some message");
+        Buffer buffer = json.toBuffer();
+        req.response().write(buffer.getBuffer(0, i));
+        req.response().write(buffer.getBuffer(i, buffer.length()));
+        req.response().write(separator);
+      }
+      req.response().end();
+    }).listen(8080, x -> {
+      if (x.succeeded()) {
+        async.complete();
+      } else {
+        tc.fail(x.cause());
+      }
+    });
+  }
+
+  @After
+  public void close(TestContext tc) {
+    vertx.close(tc.asyncAssertSuccess());
+  }
+
+
+  @Test
+  public void testSimpleStream(TestContext tc) {
+    AtomicInteger counter = new AtomicInteger();
+    Async async = tc.async();
+    JsonParser parser = JsonParser.newParser().objectValueMode()
+      .exceptionHandler(tc::fail)
+      .handler(event -> {
+        JsonObject object = event.objectValue();
+        tc.assertEquals(counter.getAndIncrement(), object.getInteger("count"));
+        tc.assertEquals("some message", object.getString("data"));
+      })
+      .endHandler(x -> async.complete());
+
+    client.get("/?separator=nl&count=10").as(BodyCodec.jsonStream(parser)).send(x -> {
+      if (x.failed()) {
+        tc.fail(x.cause());
+      }
+    });
+  }
+
+  @Test
+  public void testSimpleStreamUsingBlankLine(TestContext tc) {
+    AtomicInteger counter = new AtomicInteger();
+    Async async = tc.async();
+    JsonParser parser = JsonParser.newParser().objectValueMode()
+      .exceptionHandler(tc::fail)
+      .handler(event -> {
+        JsonObject object = event.objectValue();
+        tc.assertEquals(counter.getAndIncrement(), object.getInteger("count"));
+        tc.assertEquals("some message", object.getString("data"));
+      })
+      .endHandler(x -> async.complete());
+
+    client.get("/?separator=bl&count=10").as(BodyCodec.jsonStream(parser)).send(x -> {
+      if (x.failed()) {
+        tc.fail(x.cause());
+      }
+    });
+  }
+
+}
---
+++ vertx-web-common/src/main/java/io/vertx/ext/web/codec/impl/JsonStreamBodyCodec.java
@@ -0,0 +1,68 @@
+package io.vertx.ext.web.codec.impl;
+
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.parsetools.JsonParser;
+import io.vertx.core.streams.WriteStream;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.ext.web.codec.spi.BodyStream;
+
+import java.util.Objects;
+
+public class JsonStreamBodyCodec implements BodyCodec<Void> {
+
+  private final JsonParser parser;
+  private final StreamingBodyCodec delegate;
+
+  public JsonStreamBodyCodec(JsonParser parser) {
+    this.parser = Objects.requireNonNull(parser, "The parser must be set");
+    this.delegate = new StreamingBodyCodec(new WriteStream<Buffer>() {
+      @Override
+      public WriteStream<Buffer> exceptionHandler(Handler<Throwable> handler) {
+        parser.exceptionHandler(handler);
+        return this;
+      }
+
+      @Override
+      public WriteStream<Buffer> write(Buffer buffer) {
+        parser.handle(buffer);
+        return this;
+      }
+
+      @Override
+      public void end() {
+        parser.end();
+      }
+
+      @Override
+      public WriteStream<Buffer> setWriteQueueMaxSize(int i) {
+        return this;
+      }
+
+      @Override
+      public boolean writeQueueFull() {
+        return false;
+      }
+
+      @Override
+      public WriteStream<Buffer> drainHandler(@Nullable Handler<Void> handler) {
+        return this;
+      }
+    });
+  }
+
+  /**
+   * @return the JSON parser.
+   */
+  JsonParser getParser() {
+    return parser;
+  }
+
+
+  @Override
+  public void create(Handler<AsyncResult<BodyStream<Void>>> handler) {
+    delegate.create(handler);
+  }
+}
