ccf3d9d686a3eacb3444b90c12f4c7d7df568645
Thomas Segismont <tsegismo@redhat.com>
2019-02-15 17:45:18 +0100

    Vert.x Web GraphQL documentation

--- vertx-web-graphql/src/main/java/examples/GraphQLExamples.java
+++ vertx-web-graphql/src/main/java/examples/GraphQLExamples.java
@@ -16,9 +16,125 @@
 
 package examples;
 
+import graphql.GraphQL;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.idl.FieldWiringEnvironment;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.WiringFactory;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.graphql.GraphQLHandler;
+import io.vertx.ext.web.handler.graphql.VertxDataFetcher;
+import io.vertx.ext.web.handler.graphql.VertxPropertyDataFetcher;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
 /**
  * @author Thomas SEGISMONT
  */
 public class GraphQLExamples {
-}
 
+  public void handlerSetup(Router router) {
+    GraphQL graphQL = setupGraphQLJava();
+
+    router.route("/graphql").handler(GraphQLHandler.create(graphQL));
+  }
+
+  public void handlerSetupPost(Router router) {
+    GraphQL graphQL = setupGraphQLJava();
+
+    router.post("/graphql").handler(GraphQLHandler.create(graphQL));
+  }
+
+  private GraphQL setupGraphQLJava() {
+    return null;
+  }
+
+  class Link {}
+
+  private void completableFutureDataFetcher() {
+    DataFetcher<CompletableFuture<List<Link>>> dataFetcher = environment -> {
+      CompletableFuture<List<Link>> completableFuture = new CompletableFuture<>();
+      retrieveLinksFromBackend(environment, ar -> {
+        if (ar.succeeded()) {
+          completableFuture.complete(ar.result());
+        } else {
+          completableFuture.completeExceptionally(ar.cause());
+        }
+      });
+      return completableFuture;
+    };
+
+    RuntimeWiring runtimeWiring = newRuntimeWiring()
+      .type("Query", builder -> builder.dataFetcher("allLinks", dataFetcher))
+      .build();
+  }
+
+  private void retrieveLinksFromBackend(DataFetchingEnvironment environment, Handler<AsyncResult<List<Link>>> handler) {
+  }
+
+  private void vertxDataFetcher() {
+    VertxDataFetcher<List<Link>> dataFetcher = VertxDataFetcher.create((environment, future) -> {
+      retrieveLinksFromBackend(environment, future);
+    });
+
+    RuntimeWiring runtimeWiring = newRuntimeWiring()
+      .type("Query", builder -> builder.dataFetcher("allLinks", dataFetcher))
+      .build();
+  }
+
+  class User {}
+
+  private void routingContextInDataFetchingEnvironment() {
+    VertxDataFetcher<List<Link>> dataFetcher = VertxDataFetcher.create((environment, future) -> {
+
+      RoutingContext routingContext = environment.getContext();
+
+      User user = routingContext.get("user");
+
+      retrieveLinksPostedBy(user, future);
+    });
+  }
+
+  private void retrieveLinksPostedBy(User user, Handler<AsyncResult<List<Link>>> future) {
+  }
+
+  private void customContextInDataFetchingEnvironment(Router router) {
+    VertxDataFetcher<List<Link>> dataFetcher = VertxDataFetcher.create((environment, future) -> {
+
+      // User as custom context object
+      User user = environment.getContext();
+
+      retrieveLinksPostedBy(user, future);
+    });
+
+    GraphQL graphQL = setupGraphQLJava(dataFetcher);
+
+    // Customize the query context object when setting up the handler
+    GraphQLHandler handler = GraphQLHandler.create(graphQL).queryContext(routingContext -> {
+      return routingContext.get("user");
+    });
+
+    router.route("/graphql").handler(handler);
+  }
+
+  private GraphQL setupGraphQLJava(DataFetcher<CompletableFuture<List<Link>>> dataFetcher) {
+    return null;
+  }
+
+  private void jsonData() {
+    RuntimeWiring.Builder builder = newRuntimeWiring();
+    builder.wiringFactory(new WiringFactory() {
+      @Override
+      public DataFetcher getDefaultDataFetcher(FieldWiringEnvironment environment) {
+        return new VertxPropertyDataFetcher(environment.getFieldDefinition().getName());
+      }
+    });
+  }
+}
