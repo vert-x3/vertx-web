312f831c376690a988e2d4424047ff5d62b8cd4c
Thomas Segismont <tsegismont@gmail.com>
2019-04-23 11:39:16 +0200

    Expose GraphiQL for development (#1247)
    Closes #1202

---
+++ vertx-web-graphql/src/main/generated/io/vertx/ext/web/handler/graphql/GraphiQLOptionsConverter.java
@@ -0,0 +1,72 @@
+package io.vertx.ext.web.handler.graphql;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converter for {@link io.vertx.ext.web.handler.graphql.GraphiQLOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.web.handler.graphql.GraphiQLOptions} original class using Vert.x codegen.
+ */
+public class GraphiQLOptionsConverter {
+
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, GraphiQLOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "enabled":
+          if (member.getValue() instanceof Boolean) {
+            obj.setEnabled((Boolean)member.getValue());
+          }
+          break;
+        case "graphQLUri":
+          if (member.getValue() instanceof String) {
+            obj.setGraphQLUri((String)member.getValue());
+          }
+          break;
+        case "headers":
+          if (member.getValue() instanceof JsonObject) {
+            java.util.Map<String, java.lang.String> map = new java.util.LinkedHashMap<>();
+            ((Iterable<java.util.Map.Entry<String, Object>>)member.getValue()).forEach(entry -> {
+              if (entry.getValue() instanceof String)
+                map.put(entry.getKey(), (String)entry.getValue());
+            });
+            obj.setHeaders(map);
+          }
+          break;
+        case "query":
+          if (member.getValue() instanceof String) {
+            obj.setQuery((String)member.getValue());
+          }
+          break;
+        case "variables":
+          if (member.getValue() instanceof JsonObject) {
+            obj.setVariables(((JsonObject)member.getValue()).copy());
+          }
+          break;
+      }
+    }
+  }
+
+  public static void toJson(GraphiQLOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(GraphiQLOptions obj, java.util.Map<String, Object> json) {
+    json.put("enabled", obj.isEnabled());
+    if (obj.getGraphQLUri() != null) {
+      json.put("graphQLUri", obj.getGraphQLUri());
+    }
+    if (obj.getHeaders() != null) {
+      JsonObject map = new JsonObject();
+      obj.getHeaders().forEach((key, value) -> map.put(key, value));
+      json.put("headers", map);
+    }
+    if (obj.getQuery() != null) {
+      json.put("query", obj.getQuery());
+    }
+    if (obj.getVariables() != null) {
+      json.put("variables", obj.getVariables());
+    }
+  }
+}
---
+++ vertx-web-graphql/src/main/java/io/vertx/ext/web/handler/graphql/GraphiQLOptions.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.vertx.ext.web.handler.graphql;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.common.WebEnvironment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Embedded GraphiQL user interface options.
+ *
+ * @author Thomas Segismont
+ */
+@DataObject(generateConverter = true)
+public class GraphiQLOptions {
+
+  /**
+   * Whether GraphiQL development tool should be enabled by default = false.
+   */
+  public static final boolean DEFAULT_ENABLED = WebEnvironment.development();
+
+  private boolean enabled = DEFAULT_ENABLED;
+
+  private String graphQLUri;
+
+  private Map<String, String> headers;
+
+  private String query;
+
+  private JsonObject variables;
+
+  /**
+   * Default constructor.
+   */
+  public GraphiQLOptions() {
+  }
+
+  /**
+   * Copy constructor.
+   *
+   * @param other the options to copy
+   */
+  public GraphiQLOptions(GraphiQLOptions other) {
+    enabled = other.enabled;
+    graphQLUri = other.graphQLUri;
+    headers = other.headers == null ? null : new HashMap<>(other.headers);
+    query = other.query;
+    variables = other.variables == null ? null : other.variables.copy();
+  }
+
+  /**
+   * Constructor to create options from JSON.
+   *
+   * @param json the JSON
+   */
+  public GraphiQLOptions(JsonObject json) {
+    this();
+    GraphiQLOptionsConverter.fromJson(json, this);
+  }
+
+  /**
+   * @return a JSON representation of these options
+   */
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    GraphiQLOptionsConverter.toJson(this, json);
+    return json;
+  }
+
+  /**
+   * @return true if the GraphiQL development tool should be enabled, false otherwise
+   */
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  /**
+   * Whether the GraphiQL development tool should be enabled. Defaults to {@code false}.
+   *
+   * @param enabled true to enable the GraphiQL development tool, false otherwise
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  public GraphiQLOptions setEnabled(boolean enabled) {
+    this.enabled = enabled;
+    return this;
+  }
+
+  /**
+   * @return the GraphQL endpoint URI
+   */
+  public String getGraphQLUri() {
+    return graphQLUri;
+  }
+
+  /**
+   * Set the GraphQL endpoint URI. Defaults to the path used to get the GraphiQL user interface.
+   *
+   * @param graphQLUri the GraphQL endpoint URI
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  public GraphiQLOptions setGraphQLUri(String graphQLUri) {
+    this.graphQLUri = graphQLUri;
+    return this;
+  }
+
+  /**
+   * @return the fixed set of HTTP headers to add to GraphiQL requests
+   */
+  public Map<String, String> getHeaders() {
+    return headers;
+  }
+
+  /**
+   * A fixed set of HTTP headers to add to GraphiQL requests. Defaults to {@code null}.
+   *
+   * @param headers the set of HTTP headers to add to GraphiQL requests
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  public GraphiQLOptions setHeaders(Map<String, String> headers) {
+    this.headers = headers;
+    return this;
+  }
+
+  /**
+   * @return the query to set as initial value in the GraphiQL user interface
+   */
+  public String getQuery() {
+    return query;
+  }
+
+  /**
+   * Initial value of the query area in the GraphiQL user interface. Defaults to {@code null}.
+   *
+   * @param query the query to set as initial value
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  public GraphiQLOptions setQuery(String query) {
+    this.query = query;
+    return this;
+  }
+
+  /**
+   * @return the variables to set as initial value in the GraphiQL user interface
+   */
+  public JsonObject getVariables() {
+    return variables;
+  }
+
+  /**
+   * Initial value of the variables area in the GraphiQL user interface. Defaults to {@code null}.
+   *
+   * @param variables the variables to set as initial value
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
+  public GraphiQLOptions setVariables(JsonObject variables) {
+    this.variables = variables;
+    return this;
+  }
+}
