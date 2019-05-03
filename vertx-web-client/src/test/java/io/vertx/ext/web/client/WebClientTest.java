65bed60c01ca0c37020aa2f954ab909141872449
Liu Rui <lovearuis@gmail.com>
2019-05-03 18:39:16 +0800

    fix:issues/1114 (#1115)
    * add a method named putHeaders in HttpRequest can add headers in batches
    
    Signed-off-by: liurui <lovearuis@gmail.com>
    
    * use MultiMap.caseInsensitiveMultiMap() create headers map
    
    Signed-off-by: Liu Rui <lovearuis@gmail.com>

--- vertx-web-client/src/test/java/io/vertx/ext/web/client/WebClientTest.java
+++ vertx-web-client/src/test/java/io/vertx/ext/web/client/WebClientTest.java
@@ -135,6 +135,17 @@ public class WebClientTest extends HttpTestBase {
   }
 
   @Test
+  public void testPutHeaders() throws Exception {
+    MultiMap headers = MultiMap.caseInsensitiveMultiMap();
+    headers.add("foo","bar");
+    headers.add("ping","pong");
+    testRequest(client -> client.get("somehost", "somepath").putHeaders(headers), req -> {
+      assertEquals("bar", req.headers().get("foo"));
+      assertEquals("pong", req.headers().get("ping"));
+    });
+  }
+
+  @Test
   public void testUserAgentHeaderRemoved() throws Exception {
     testRequest(client -> {
       HttpRequest<Buffer> request = client.get("somehost", "somepath");
