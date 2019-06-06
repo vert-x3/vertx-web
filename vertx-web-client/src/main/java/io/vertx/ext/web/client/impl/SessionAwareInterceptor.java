8894ab9b617456c151b7bfd5f3b57db34c848ed6
slinkydeveloper <francescoguard@gmail.com>
2019-06-06 22:30:49 +0200

    Cookies configured during redirects are correctly managed
    (cherry picked from commit 31e34db64ba265e179ea43e604ebe673cfe0bdee)

--- vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
@@ -1,5 +1,6 @@
 package io.vertx.ext.web.client.impl;
 
+import java.net.URI;
 import java.util.List;
 
 import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
@@ -8,6 +9,8 @@ import io.netty.handler.codec.http.cookie.Cookie;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.CaseInsensitiveHeaders;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.spi.CookieStore;
 
 /**
@@ -22,6 +25,9 @@ public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
     switch(context.phase()) {
     case PREPARE_REQUEST:
       prepareRequest(context);
+      break;
+    case FOLLOW_REDIRECT:
+      processRedirectCookies(context);
       break;
     case DISPATCH_RESPONSE:
       processResponse(context);
@@ -59,8 +65,67 @@ public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
     }
   }
 
+  private void processRedirectCookies(HttpContext<?> context) {
+    this.processRedirectResponse(context);
+    this.prepareRedirectRequest(context);
+  }
+
+  private void processRedirectResponse(HttpContext<?> context) {
+    // Now the context contains the redirect request in clientRequest() and the original request in request()
+    List<String> cookieHeaders = context.clientResponse().cookies();
+    if (cookieHeaders == null) {
+      return;
+    }
+
+    WebClientSessionAware webclient = (WebClientSessionAware) ((HttpRequestImpl)context.request()).client;
+    HttpRequestImpl<?> originalRequest = (HttpRequestImpl<?>) context.request();
+    CookieStore cookieStore = webclient.cookieStore();
+    String domain = URI.create(context.clientResponse().request().absoluteURI()).getHost();
+    if (domain.equals(originalRequest.host) && originalRequest.virtualHost != null) {
+      domain = originalRequest.virtualHost;
+    }
+    final String finalDomain = domain;
+    cookieHeaders.forEach(header -> {
+      Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
+      if (cookie != null) {
+        if (cookie.domain() == null) {
+          // Set the domain if missing, because we need to send cookies
+          // only to the domains we received them from.
+          cookie.setDomain(finalDomain);
+        }
+        cookieStore.put(cookie);
+      }
+    });
+  }
+
+  private void prepareRedirectRequest(HttpContext<?> context) {
+    // Now the context contains the redirect request in clientRequest() and the original request in request()
+    HttpClientRequest redirectRequest = context.clientRequest();
+    HttpRequestImpl<?> originalRequest = (HttpRequestImpl<?>) context.request();
+    WebClientSessionAware webclient = (WebClientSessionAware) originalRequest.client;
+
+    MultiMap headers = context.get(HEADERS_CONTEXT_KEY);
+    if (headers == null) {
+      headers = new CaseInsensitiveHeaders().addAll(redirectRequest.headers());
+      context.set(SessionAwareInterceptor.HEADERS_CONTEXT_KEY, headers);
+    }
+
+    String redirectHost = URI.create(redirectRequest.absoluteURI()).getHost();
+    String domain;
+    if (redirectHost.equals(originalRequest.host) && originalRequest.virtualHost != null) {
+      domain = originalRequest.virtualHost;
+    } else {
+      domain = redirectHost;
+    }
+
+    Iterable<Cookie> cookies = webclient.cookieStore().get(originalRequest.ssl, domain, redirectRequest.path());
+    for (Cookie c : cookies) {
+      redirectRequest.headers().add("cookie", ClientCookieEncoder.STRICT.encode(c));
+    }
+  }
+
   private void processResponse(HttpContext<?> context) {
-    List<String> cookieHeaders = context.response().cookies();
+    List<String> cookieHeaders = context.clientResponse().cookies();
     if (cookieHeaders == null) {
       return;
     }
