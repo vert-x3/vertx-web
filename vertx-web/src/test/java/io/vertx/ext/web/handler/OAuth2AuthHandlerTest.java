2fcd797c0ef9da628b29c7c0204f2f3fdef68384
Julien Viet <julien@julienviet.com>
2019-08-13 15:14:06 +0200

    Fix broken tests

--- vertx-web/src/test/java/io/vertx/ext/web/handler/OAuth2AuthHandlerTest.java
+++ vertx-web/src/test/java/io/vertx/ext/web/handler/OAuth2AuthHandlerTest.java
@@ -56,7 +56,7 @@ public class OAuth2AuthHandlerTest extends WebTestBase {
   public void testAuthCodeFlow() throws Exception {
 
     // lets mock a oauth2 server using code auth code flow
-    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
+    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, new OAuth2ClientOptions()
         .setClientID("client-id")
         .setClientSecret("client-secret")
         .setSite("http://localhost:10000"));
@@ -113,7 +113,7 @@ public class OAuth2AuthHandlerTest extends WebTestBase {
   public void testAuthCodeFlowBadSetup() throws Exception {
 
     // lets mock a oauth2 server using code auth code flow
-    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
+    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, new OAuth2ClientOptions()
       .setClientID("client-id")
       .setClientSecret("client-secret")
       .setSite("http://localhost:10000"));
@@ -189,10 +189,11 @@ public class OAuth2AuthHandlerTest extends WebTestBase {
   public void testPasswordFlow() throws Exception {
 
     // lets mock a oauth2 server using code auth code flow
-    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.PASSWORD, new OAuth2ClientOptions()
+    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, new OAuth2ClientOptions()
       .setClientID("client-id")
       .setClientSecret("client-secret")
-      .setSite("http://localhost:10000"));
+      .setSite("http://localhost:10000")
+      .setFlow(OAuth2FlowType.PASSWORD));
 
     final CountDownLatch latch = new CountDownLatch(1);
 
@@ -245,7 +246,7 @@ public class OAuth2AuthHandlerTest extends WebTestBase {
   public void testBearerOnly() throws Exception {
 
     // lets mock a oauth2 server using code auth code flow
-    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions().setClientID("client-id"));
+    OAuth2Auth oauth2 = OAuth2Auth.create(vertx, new OAuth2ClientOptions().setClientID("client-id"));
     OAuth2AuthHandler oauth2Handler = OAuth2AuthHandler.create(oauth2);
 
     // protect everything under /protected
