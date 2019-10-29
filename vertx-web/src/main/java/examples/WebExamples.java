de15259611b848d2bd0600618c5f00f02c68a236
Paulo Lopes <pmlopes@gmail.com>
2019-10-29 12:57:52 +0100

    Fix #1427: Update docs to avoid deprecated API

--- vertx-web/src/main/java/examples/WebExamples.java
+++ vertx-web/src/main/java/examples/WebExamples.java
@@ -896,9 +896,8 @@ public class WebExamples {
 
     SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
     BridgeOptions options = new BridgeOptions();
-    sockJSHandler.bridge(options);
-
-    router.route("/eventbus/*").handler(sockJSHandler);
+    // mount the bridge on the router
+    router.mountSubRouter("/eventbus", sockJSHandler.bridge(options));
   }
 
   public void example46(Vertx vertx) {
@@ -937,9 +936,8 @@ public class WebExamples {
       addOutboundPermitted(outboundPermitted1).
       addOutboundPermitted(outboundPermitted2);
 
-    sockJSHandler.bridge(options);
-
-    router.route("/eventbus/*").handler(sockJSHandler);
+    // mount the bridge on the router
+    router.mountSubRouter("/eventbus", sockJSHandler.bridge(options));
   }
 
   public void example47() {
@@ -964,8 +962,6 @@ public class WebExamples {
     inboundPermitted.setRequiredAuthority("place_orders");
 
     SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
-    sockJSHandler.bridge(new BridgeOptions().
-      addInboundPermitted(inboundPermitted));
 
     // Now set up some basic auth handling:
 
@@ -975,9 +971,10 @@ public class WebExamples {
 
     router.route("/eventbus/*").handler(basicAuthHandler);
 
-
-    router.route("/eventbus/*").handler(sockJSHandler);
-
+    // mount the bridge on the router
+    router.mountSubRouter(
+      "/eventbus",
+      sockJSHandler.bridge(new BridgeOptions().addInboundPermitted(inboundPermitted)));
   }
 
   public void example48_1(Vertx vertx) {
@@ -990,20 +987,19 @@ public class WebExamples {
     SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
     BridgeOptions options = new BridgeOptions().addInboundPermitted(inboundPermitted);
 
-    sockJSHandler.bridge(options, be -> {
-      if (be.type() == BridgeEventType.PUBLISH || be.type() == BridgeEventType.SEND) {
-        // Add some headers
-        JsonObject headers = new JsonObject().put("header1", "val").put("header2", "val2");
-        JsonObject rawMessage = be.getRawMessage();
-        rawMessage.put("headers", headers);
-        be.setRawMessage(rawMessage);
-      }
-      be.complete(true);
-    });
-
-    router.route("/eventbus/*").handler(sockJSHandler);
-
-
+    // mount the bridge on the router
+    router.mountSubRouter(
+      "/eventbus",
+      sockJSHandler.bridge(options, be -> {
+        if (be.type() == BridgeEventType.PUBLISH || be.type() == BridgeEventType.SEND) {
+          // Add some headers
+          JsonObject headers = new JsonObject().put("header1", "val").put("header2", "val2");
+          JsonObject rawMessage = be.getRawMessage();
+          rawMessage.put("headers", headers);
+          be.setRawMessage(rawMessage);
+        }
+        be.complete(true);
+      }));
   }
 
   public void example49(Vertx vertx) {
@@ -1016,7 +1012,8 @@ public class WebExamples {
     SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
     BridgeOptions options = new BridgeOptions().addInboundPermitted(inboundPermitted);
 
-    sockJSHandler.bridge(options, be -> {
+    // mount the bridge on the router
+    router.mountSubRouter("/eventbus", sockJSHandler.bridge(options, be -> {
       if (be.type() == BridgeEventType.PUBLISH || be.type() == BridgeEventType.RECEIVE) {
         if (be.getRawMessage().getString("body").equals("armadillos")) {
           // Reject it
@@ -1025,11 +1022,7 @@ public class WebExamples {
         }
       }
       be.complete(true);
-    });
-
-    router.route("/eventbus/*").handler(sockJSHandler);
-
-
+    }));
   }
 
   public void handleSocketIdle(Vertx vertx, PermittedOptions inboundPermitted) {
@@ -1039,15 +1032,14 @@ public class WebExamples {
     SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
     BridgeOptions options = new BridgeOptions().addInboundPermitted(inboundPermitted).setPingTimeout(5000);
 
-    sockJSHandler.bridge(options, be -> {
+    // mount the bridge on the router
+    router.mountSubRouter("/eventbus", sockJSHandler.bridge(options, be -> {
       if (be.type() == BridgeEventType.SOCKET_IDLE) {
         // Do some custom handling...
       }
 
       be.complete(true);
-    });
-
-    router.route("/eventbus/*").handler(sockJSHandler);
+    }));
   }
 
   public void example50(Vertx vertx) {
@@ -1285,22 +1277,22 @@ public class WebExamples {
       .putHeader("Content-Type", "text/html")
       .end(
         "<html>\n" +
-        "  <body>\n" +
-        "    <p>\n" +
-        "      Well, hello there!\n" +
-        "    </p>\n" +
-        "    <p>\n" +
-        "      We're going to the protected resource, if there is no\n" +
-        "      user in the session we will talk to the GitHub API. Ready?\n" +
-        "      <a href=\"/protected\">Click here</a> to begin!</a>\n" +
-        "    </p>\n" +
-        "    <p>\n" +
-        "      <b>If that link doesn't work</b>, remember to provide\n" +
-        "      your own <a href=\"https://github.com/settings/applications/new\">\n" +
-        "      Client ID</a>!\n" +
-        "    </p>\n" +
-        "  </body>\n" +
-        "</html>"));
+          "  <body>\n" +
+          "    <p>\n" +
+          "      Well, hello there!\n" +
+          "    </p>\n" +
+          "    <p>\n" +
+          "      We're going to the protected resource, if there is no\n" +
+          "      user in the session we will talk to the GitHub API. Ready?\n" +
+          "      <a href=\"/protected\">Click here</a> to begin!</a>\n" +
+          "    </p>\n" +
+          "    <p>\n" +
+          "      <b>If that link doesn't work</b>, remember to provide\n" +
+          "      your own <a href=\"https://github.com/settings/applications/new\">\n" +
+          "      Client ID</a>!\n" +
+          "    </p>\n" +
+          "  </body>\n" +
+          "</html>"));
     // The protected resource
     router.get("/protected").handler(ctx -> {
       // at this moment your user object should contain the info
