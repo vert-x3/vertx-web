4ebb462a0f1a6a5799b6c953195b004638676885
Dimitris Zenios <dimitris.zenios@gmail.com>
2019-11-18 11:46:59 +0200

    Add copyright compliance

--- vertx-web/src/main/java/io/vertx/ext/web/impl/ForwardedParser.java
+++ vertx-web/src/main/java/io/vertx/ext/web/impl/ForwardedParser.java
@@ -1,5 +1,22 @@
-//Heavily influenced from spring forward header parser
-//https://github.com/spring-projects/spring-framework/blob/master/spring-web/src/main/java/org/springframework/web/util/UriComponentsBuilder.java#L849
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+
+// This code was Heavily influenced from spring forward header parser
+// https://github.com/spring-projects/spring-framework/blob/master/spring-web/src/main/java/org/springframework/web/util/UriComponentsBuilder.java#L849
+
 package io.vertx.ext.web.impl;
 
 import io.netty.util.AsciiString;
