2ad8ed86e5f324230ca16d353946a9a19a375000
Dimitris Zenios <dimitris.zenios@gmail.com>
2019-11-18 17:15:38 +0200

    Removed unused @code annotation

--- vertx-web/src/main/java/io/vertx/ext/web/Router.java
+++ vertx-web/src/main/java/io/vertx/ext/web/Router.java
@@ -394,7 +394,7 @@ public interface Router extends Handler<HttpServerRequest> {
   /**
    * Set whether the router should parse "forwarded"-type headers
    *
-   * @param allow {@code false} to enable parsing of "forwarded"-type headers
+   * @param allow to enable parsing of "forwarded"-type headers
    * @return a reference to this, so the API can be used fluently
    */
   @Fluent
