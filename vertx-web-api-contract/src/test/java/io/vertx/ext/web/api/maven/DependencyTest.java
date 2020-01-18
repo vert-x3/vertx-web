b30591eb788abfce7315ad9990daa67090d35a79
Michael Clausen <encodeering@gmail.com>
2020-01-18 10:51:44 +0100

    removes guava and findbugs from api contract module
    * swagger doesn't define these transitive dependencies anymore and a cve is reported for guava
    
    Signed-off-by: Michael Clausen <encodeering@gmail.com>

--- vertx-web-api-contract/src/test/java/io/vertx/ext/web/api/maven/DependencyTest.java
+++ vertx-web-api-contract/src/test/java/io/vertx/ext/web/api/maven/DependencyTest.java
@@ -2,18 +2,18 @@ package io.vertx.ext.web.api.maven;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class DependencyTest {
 
   @Test
-  public void testGuavaDependency () throws Exception {
-    assertNotNull (getClass ().getResource ("/META-INF/maven/com.google.guava/guava/pom.properties"));
+  public void testNoGuavaDependency () throws Exception {
+    assertNull (getClass ().getResource ("/META-INF/maven/com.google.guava/guava/pom.properties"));
   }
 
   @Test
-  public void testFirebugDependency () throws Exception {
-    assertNotNull (getClass ().getResource ("/META-INF/maven/com.google.code.findbugs/jsr305"));
+  public void testNoFirebugDependency () throws Exception {
+    assertNull (getClass ().getResource ("/META-INF/maven/com.google.code.findbugs/jsr305"));
   }
 
 }
