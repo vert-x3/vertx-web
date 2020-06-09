8891fdaba6e3ec891b5e99c08c2113be6dd2cdd2
slinkydeveloper <francescoguard@gmail.com>
2020-06-09 16:07:24 +0200

    Fixed docs
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ vertx-web-openapi/src/main/generated/io/vertx/ext/web/openapi/RouterFactoryOptionsConverter.java
@@ -0,0 +1,51 @@
+package io.vertx.ext.web.openapi;
+
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Converter and mapper for {@link io.vertx.ext.web.openapi.RouterFactoryOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.web.openapi.RouterFactoryOptions} original class using Vert.x codegen.
+ */
+public class RouterFactoryOptionsConverter {
+
+
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, RouterFactoryOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "mountNotImplementedHandler":
+          if (member.getValue() instanceof Boolean) {
+            obj.setMountNotImplementedHandler((Boolean)member.getValue());
+          }
+          break;
+        case "mountResponseContentTypeHandler":
+          if (member.getValue() instanceof Boolean) {
+            obj.setMountResponseContentTypeHandler((Boolean)member.getValue());
+          }
+          break;
+        case "operationModelKey":
+          if (member.getValue() instanceof String) {
+            obj.setOperationModelKey((String)member.getValue());
+          }
+          break;
+        case "requireSecurityHandlers":
+          if (member.getValue() instanceof Boolean) {
+            obj.setRequireSecurityHandlers((Boolean)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+   static void toJson(RouterFactoryOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(RouterFactoryOptions obj, java.util.Map<String, Object> json) {
+    json.put("mountNotImplementedHandler", obj.isMountNotImplementedHandler());
+    json.put("mountResponseContentTypeHandler", obj.isMountResponseContentTypeHandler());
+    if (obj.getOperationModelKey() != null) {
+      json.put("operationModelKey", obj.getOperationModelKey());
+    }
+    json.put("requireSecurityHandlers", obj.isRequireSecurityHandlers());
+  }
+}
