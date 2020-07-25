b455755c6db02212cf63eabcb61e9387e67c5c6e
Arniu Tseng <arniu2006@gmail.com>
2020-07-25 15:35:13 +0800

    Format code

--- vertx-web-api-contract/src/main/java/io/vertx/ext/web/api/validation/ParameterType.java
+++ vertx-web-api-contract/src/main/java/io/vertx/ext/web/api/validation/ParameterType.java
@@ -17,30 +17,40 @@ public enum ParameterType {
   /**
    * STRING Type accept every string
    */
-  GENERIC_STRING(RequestParameter::create), EMAIL(new StringTypeValidator(RegularExpressions.EMAIL)),
-  URI(new StringTypeValidator(RegularExpressions.URI)), /**
+  GENERIC_STRING(RequestParameter::create),
+  EMAIL(new StringTypeValidator(RegularExpressions.EMAIL)),
+  URI(new StringTypeValidator(RegularExpressions.URI)),
+  /**
    * It allows true, false, t, f, 1, 0
    */
-  BOOL(new BooleanTypeValidator(null)), /**
+  BOOL(new BooleanTypeValidator(null)),
+  /**
    * INT type does the validation with Integer.parseInt(value)
    */
-  INT(ParameterTypeValidator.createIntegerTypeValidator(null)), /**
+  INT(ParameterTypeValidator.createIntegerTypeValidator(null)),
+  /**
    * FLOAT type does the validation with Float.parseFloat(value)
    */
-  FLOAT(ParameterTypeValidator.createFloatTypeValidator(null)), /**
+  FLOAT(ParameterTypeValidator.createFloatTypeValidator(null)),
+  /**
    * DOUBLE type does the validation with Double.parseDouble(value)
    */
-  DOUBLE(ParameterTypeValidator.createDoubleTypeValidator(null)), /**
+  DOUBLE(ParameterTypeValidator.createDoubleTypeValidator(null)),
+  /**
    * DATE as defined by full-date - RFC3339
    */
-  DATE(new StringTypeValidator(RegularExpressions.DATE)), /**
+  DATE(new StringTypeValidator(RegularExpressions.DATE)),
+  /**
    * DATETIME as defined by date-time - RFC3339
    */
-  DATETIME(new StringTypeValidator(RegularExpressions.DATETIME)), /**
+  DATETIME(new StringTypeValidator(RegularExpressions.DATETIME)),
+  /**
    * TIME as defined by partial-time - RFC3339
    */
-  TIME(new StringTypeValidator(RegularExpressions.TIME)), BASE64(new StringTypeValidator(RegularExpressions.BASE64)),
-  IPV4(new StringTypeValidator(RegularExpressions.IPV4)), IPV6(new StringTypeValidator(RegularExpressions.IPV6)),
+  TIME(new StringTypeValidator(RegularExpressions.TIME)),
+  BASE64(new StringTypeValidator(RegularExpressions.BASE64)),
+  IPV4(new StringTypeValidator(RegularExpressions.IPV4)),
+  IPV6(new StringTypeValidator(RegularExpressions.IPV6)),
   HOSTNAME(new StringTypeValidator(RegularExpressions.HOSTNAME)),
 
   /**
