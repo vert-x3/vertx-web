03a82a04603fd4c7e51cc6cfc82625ecc6ed1336
slinkydeveloper <francescoguard@gmail.com>
2020-07-28 11:50:34 +0200

    Fix
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- vertx-web-api-contract/src/main/java/io/vertx/ext/web/api/package-info.java
+++ vertx-web-api-contract/src/main/java/io/vertx/ext/web/api/package-info.java
@@ -2,7 +2,6 @@
  * @deprecated You should use the new module vertx-web-openapi
  */
 @ModuleGen(name = "vertx-web-api-contract", groupPackage = "io.vertx")
-@Deprecated
 package io.vertx.ext.web.api;
 
 import io.vertx.codegen.annotations.ModuleGen;
