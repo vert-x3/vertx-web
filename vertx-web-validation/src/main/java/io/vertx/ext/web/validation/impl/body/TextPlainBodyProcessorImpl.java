d0a6a7d5460cd2cb4bf75b2f8dcc9261b6cd3358
slinkydeveloper <francescoguard@gmail.com>
2020-11-09 15:42:38 +0100

    Fix #1772
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- vertx-web-validation/src/main/java/io/vertx/ext/web/validation/impl/body/TextPlainBodyProcessorImpl.java
+++ vertx-web-validation/src/main/java/io/vertx/ext/web/validation/impl/body/TextPlainBodyProcessorImpl.java
@@ -1,7 +1,10 @@
 package io.vertx.ext.web.validation.impl.body;
 
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.validation.BodyProcessorException;
+import io.vertx.ext.web.validation.MalformedValueException;
 import io.vertx.ext.web.validation.RequestParameter;
 import io.vertx.ext.web.validation.impl.validator.ValueValidator;
 
@@ -20,6 +23,13 @@ public class TextPlainBodyProcessorImpl implements BodyProcessor {
 
   @Override
   public Future<RequestParameter> process(RoutingContext requestContext) {
-    return valueValidator.validate(requestContext.getBodyAsString());
+    String body = requestContext.getBodyAsString();
+    if (body == null) {
+      throw BodyProcessorException.createParsingError(
+        requestContext.request().getHeader(HttpHeaders.CONTENT_TYPE),
+        new MalformedValueException("Null body")
+      );
+    }
+    return valueValidator.validate(body);
   }
 }
