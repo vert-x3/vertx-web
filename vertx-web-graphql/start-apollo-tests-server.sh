a37408e8033b6b0f2b9586cdf621da86f3951397
Thomas Segismont <tsegismont@gmail.com>
2020-11-16 11:27:20 +0100

    GH Actions: compile tests before starting Apollo test server
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-web-graphql/start-apollo-tests-server.sh
+++ vertx-web-graphql/start-apollo-tests-server.sh
@@ -2,7 +2,7 @@
 
 MSG="Apollo tests server started"
 
-mvn exec:java -Dexec.mainClass=io.vertx.ext.web.handler.graphql.ApolloTestsServer -Dexec.classpathScope=test > "${HOME}"/ApolloTestsServer.log 2>&1 &
+mvn test-compile exec:java -Dexec.mainClass=io.vertx.ext.web.handler.graphql.ApolloTestsServer -Dexec.classpathScope=test > "${HOME}"/ApolloTestsServer.log 2>&1 &
 echo $! > "${HOME}"/ApolloTestsServer.pid
 ( tail -f "${HOME}"/ApolloTestsServer.log & ) | grep -q "${MSG}"
 echo "${MSG}"
