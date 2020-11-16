71a0e1b1181c792978298d50cbf192b83d2f97b1
Thomas Segismont <tsegismont@gmail.com>
2020-11-16 11:38:11 +0100

    GH Actions: add SockJS WriteHandler Tests
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ vertx-web/start-sockjs-write-handler-tests-server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+MSG="Apollo tests server started"
+
+mvn test-compile exec:java -Dexec.mainClass=io.vertx.ext.web.handler.sockjs.SockJSWriteHandlerTestServer -Dexec.classpathScope=test > "${HOME}"/SockJSWriteHandlerTestServer.log 2>&1 &
+echo $! > "${HOME}"/SockJSWriteHandlerTestServer.pid
+( tail -f "${HOME}"/SockJSWriteHandlerTestServer.log & ) | grep -q "${MSG}"
+echo "${MSG}"
