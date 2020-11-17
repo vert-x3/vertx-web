f74d1e864d456d9ffcc005153c91909af2121888
Julien Viet <julien@julienviet.com>
2020-11-17 15:46:22 +0100

    GraphQL Apollo test should compile all snapshots it uses otherwise it will download snapshots
    Signed-off-by: Julien Viet <julien@julienviet.com>

--- vertx-web-graphql/start-apollo-tests-server.sh
+++ vertx-web-graphql/start-apollo-tests-server.sh
@@ -2,7 +2,8 @@
 
 MSG="Apollo tests server started"
 
-mvn test-compile exec:java -Dexec.mainClass=io.vertx.ext.web.handler.graphql.ApolloTestsServer -Dexec.classpathScope=test > "${HOME}"/ApolloTestsServer.log 2>&1 &
+mvn -am -pl vertx-web-graphql install -DskipTests
+mvn -pl vertx-web-graphql exec:java -Dexec.mainClass=io.vertx.ext.web.handler.graphql.ApolloTestsServer -Dexec.classpathScope=test > "${HOME}"/ApolloTestsServer.log 2>&1 &
 echo $! > "${HOME}"/ApolloTestsServer.pid
 ( tail -f "${HOME}"/ApolloTestsServer.log & ) | grep -q "${MSG}"
 echo "${MSG}"
