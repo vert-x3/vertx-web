d1a89f46bbbcb3e59c0b8b91954a18d8085dec9d
Thomas Segismont <tsegismont@gmail.com>
2020-12-04 14:49:13 +0100

    CI: Always delete Apollo and SockJS test server files
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-web-graphql/stop-apollo-tests-server.sh
+++ vertx-web-graphql/stop-apollo-tests-server.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-(pkill -9 -F "${HOME}"/ApolloTestsServer.pid 2>/dev/null) || (rm -f "${HOME}"/ApolloTestsServer.pid && rm -f "${HOME}"/ApolloTestsServer.log)
+pkill -9 -F "${HOME}"/ApolloTestsServer.pid 2>/dev/null
+rm -f "${HOME}"/ApolloTestsServer.pid
+rm -f "${HOME}"/ApolloTestsServer.log
