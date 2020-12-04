d15528ff8b67ca6a4cf294496c885fcd722944e0
Thomas Segismont <tsegismont@gmail.com>
2020-12-04 14:51:21 +0100

    CI: Build deps before starting SockJS WriteHandler Tests server
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-web/start-sockjs-write-handler-tests-server.sh
+++ vertx-web/start-sockjs-write-handler-tests-server.sh
@@ -2,7 +2,8 @@
 
 MSG="SockJS writeHandler tests server started"
 
-mvn test-compile exec:java -Dexec.mainClass=io.vertx.ext.web.handler.sockjs.SockJSWriteHandlerTestServer -Dexec.classpathScope=test > "${HOME}"/SockJSWriteHandlerTestServer.log 2>&1 &
+mvn -am -pl vertx-web install -DskipTests
+mvn -pl vertx-web exec:java -Dexec.mainClass=io.vertx.ext.web.handler.sockjs.SockJSWriteHandlerTestServer -Dexec.classpathScope=test > "${HOME}"/SockJSWriteHandlerTestServer.log 2>&1 &
 echo $! > "${HOME}"/SockJSWriteHandlerTestServer.pid
 ( tail -f "${HOME}"/SockJSWriteHandlerTestServer.log & ) | grep -q "${MSG}"
 echo "${MSG}"
