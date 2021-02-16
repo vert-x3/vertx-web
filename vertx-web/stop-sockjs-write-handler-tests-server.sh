42218f1ef59ccaca72f95bdd9244a305e19cc017
Paulo Lopes <pmlopes@gmail.com>
2021-02-16 12:50:13 +0100

    Add missing CI scripts
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

---
+++ vertx-web/stop-sockjs-write-handler-tests-server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+pkill -9 -F "${HOME}"/SockJSWriteHandlerTestServer.pid 2>/dev/null
+rm -f "${HOME}"/SockJSWriteHandlerTestServer.pid
+rm -f "${HOME}"/SockJSWriteHandlerTestServer.log
