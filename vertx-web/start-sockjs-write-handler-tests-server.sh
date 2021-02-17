cf01fc6942c9e1855d3926bc588981cea4a997f5
Paulo Lopes <pmlopes@gmail.com>
2021-02-17 10:52:16 +0100

    Remove SockJS WriteTests as they were 4.0 only
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- vertx-web/start-sockjs-write-handler-tests-server.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-MSG="SockJS writeHandler tests server started"
-
-mvn -am -pl vertx-web install -DskipTests
-mvn -pl vertx-web exec:java -Dexec.mainClass=io.vertx.ext.web.handler.sockjs.SockJSWriteHandlerTestServer -Dexec.classpathScope=test > "${HOME}"/SockJSWriteHandlerTestServer.log 2>&1 &
-echo $! > "${HOME}"/SockJSWriteHandlerTestServer.pid
-( tail -f "${HOME}"/SockJSWriteHandlerTestServer.log & ) | grep -q "${MSG}"
-echo "${MSG}"
