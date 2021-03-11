cb3f2cd094132cecef095ee74c819b1d9403b207
Paulo Lopes <pmlopes@gmail.com>
2021-03-11 17:13:23 +0100

    Add commit from: https://github.com/casid/vertx-web/commit/4664720a6b507fd62d96057a08307ce0576c675a
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- vertx-template-engines/vertx-web-templ-jte/src/main/java/io/vertx/ext/web/templ/jte/impl/JteTemplateEngineImpl.java
+++ vertx-template-engines/vertx-web-templ-jte/src/main/java/io/vertx/ext/web/templ/jte/impl/JteTemplateEngineImpl.java
@@ -18,7 +18,7 @@ package io.vertx.ext.web.templ.jte.impl;
 
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
-import gg.jte.output.StringOutput;
+import gg.jte.output.Utf8ByteOutput;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -55,6 +55,7 @@ public class JteTemplateEngineImpl implements JteTemplateEngine {
   public JteTemplateEngineImpl(Vertx vertx, String templateRootDirectory) {
     codeResolver = new VertxDirectoryCodeResolver(vertx, templateRootDirectory);
     templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+    templateEngine.setBinaryStaticContent(true);
   }
 
   public JteTemplateEngineImpl() {
@@ -65,9 +66,9 @@ public class JteTemplateEngineImpl implements JteTemplateEngine {
   @Override
   public void render(Map<String, Object> context, String templateFile, Handler<AsyncResult<Buffer>> handler) {
     try {
-      StringOutput output = new StringOutput();
+      Utf8ByteOutput output = new Utf8ByteOutput();
       templateEngine.render(templateFile, context, output);
-      handler.handle(Future.succeededFuture(Buffer.buffer(output.toString())));
+      handler.handle(Future.succeededFuture(JteBufferUtil.toBuffer(output)));
     } catch (RuntimeException ex) {
       handler.handle(Future.failedFuture(ex));
     }
