8c6185ea660a5747ed88d225ab450d442aeb1bf9
Julien Viet <julien@julienviet.com>
2021-10-27 10:08:26 +0200

    Regen converters

--- vertx-web-api-service/src/main/generated/io/vertx/ext/web/api/service/ServiceResponseConverter.java
+++ vertx-web-api-service/src/main/generated/io/vertx/ext/web/api/service/ServiceResponseConverter.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.impl.JsonUtil;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.ext.web.api.service.ServiceResponse}.
@@ -13,12 +14,15 @@ import java.time.format.DateTimeFormatter;
 public class ServiceResponseConverter {
 
 
+  private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
+  private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
+
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ServiceResponse obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "payload":
           if (member.getValue() instanceof String) {
-            obj.setPayload(io.vertx.core.buffer.Buffer.buffer(JsonUtil.BASE64_DECODER.decode((String)member.getValue())));
+            obj.setPayload(io.vertx.core.buffer.Buffer.buffer(BASE64_DECODER.decode((String)member.getValue())));
           }
           break;
         case "statusCode":
@@ -41,7 +45,7 @@ public class ServiceResponseConverter {
 
    static void toJson(ServiceResponse obj, java.util.Map<String, Object> json) {
     if (obj.getPayload() != null) {
-      json.put("payload", JsonUtil.BASE64_ENCODER.encodeToString(obj.getPayload().getBytes()));
+      json.put("payload", BASE64_ENCODER.encodeToString(obj.getPayload().getBytes()));
     }
     if (obj.getStatusCode() != null) {
       json.put("statusCode", obj.getStatusCode());
