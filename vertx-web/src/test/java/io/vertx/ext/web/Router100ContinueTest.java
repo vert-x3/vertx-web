91ea3dbe4854924256004258a5b632dcad80aa30
Paulo Lopes <pmlopes@gmail.com>
2022-04-06 12:45:08 +0200

    WiP
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

---
+++ vertx-web/src/test/java/io/vertx/ext/web/Router100ContinueTest.java
@@ -0,0 +1,89 @@
+package io.vertx.ext.web;
+
+import io.vertx.core.http.*;
+import io.vertx.ext.auth.authorization.RoleBasedAuthorization;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.oauth2.OAuth2Options;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.handler.*;
+import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.ext.web.sstore.SessionStore;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class Router100ContinueTest {
+
+  @Rule
+  public final RunTestOnContext rule = new RunTestOnContext();
+
+  final Router router = Router.router(rule.vertx());
+
+  HttpServer server;
+  HttpClient client;
+
+  @Before
+  public void setup(TestContext should) {
+    final Async setup = should.async();
+    rule.vertx()
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(0)
+      .onSuccess(server -> {
+        this.server = server;
+        this.client = rule.vertx()
+          .createHttpClient(new HttpClientOptions().setDefaultPort(server.actualPort()).setDefaultHost("localhost"));
+        setup.complete();
+      })
+      .onFailure(should::fail);
+  }
+
+  @Test
+  public void testContinue(TestContext should) {
+    final Async test = should.async();
+    router.route()
+      .handler((PlatformHandler) ctx -> {
+        should.assertEquals("100-continue", ctx.request().getHeader(HttpHeaders.EXPECT));
+        // Send a 100 continue response
+        ctx.response().writeContinue();
+
+        // The client should send the body when it receives the 100 response
+        ctx.request().bodyHandler(body -> {
+          // Do something with body
+        });
+
+        ctx.request().endHandler(v -> {
+          ctx.response().end();
+        });
+
+      });
+
+    client.request(HttpMethod.POST, "/")
+      .onFailure(should::fail)
+      .onSuccess(req -> {
+        req
+          .putHeader(HttpHeaders.EXPECT, "100-continue")
+          .continueHandler(v -> {
+            req
+              .end("DATA")
+              .onFailure(should::fail);
+          })
+          .sendHead()
+          .onFailure(should::fail);
+
+        req
+          .response()
+          .onFailure(should::fail)
+          .onComplete(res -> {
+            test.complete();
+          });
+      });
+  }
+
+}
