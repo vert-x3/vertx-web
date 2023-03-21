fe668a86309388f2703f4b9302869de49650a75a
Julien Viet <julien@julienviet.com>
2023-03-21 14:42:52 +0100

    Swagger v3 should not appear in API

---
+++ vertx-web-api-contract/src/main/java/io/vertx/ext/web/api/contract/openapi3/impl/Utils.java
@@ -0,0 +1,45 @@
+package io.vertx.ext.web.api.contract.openapi3.impl;
+
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.ResolverCache;
+import io.swagger.v3.parser.core.models.AuthorizationValue;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.api.contract.RouterFactoryException;
+import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Utils {
+
+  public static Future<OpenAPI3RouterFactory> create(Vertx vertx, String url, List<JsonObject> auth) {
+    Promise<OpenAPI3RouterFactory> promise = Promise.promise();
+
+    List<AuthorizationValue> authorizationValues = auth.stream()
+      .map(obj -> {
+        AuthorizationValue authorizationValue = obj.mapTo(AuthorizationValue.class);
+        authorizationValue.setUrlMatcher(u -> true);
+        return authorizationValue;
+      })
+      .collect(Collectors.toList());
+    vertx.executeBlocking((Promise<OpenAPI3RouterFactory> future) -> {
+      SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser()
+        .readLocation(url, authorizationValues, OpenApi3Utils.getParseOptions());
+      if (swaggerParseResult.getMessages().isEmpty()) {
+        future.complete(new OpenAPI3RouterFactoryImpl(vertx, swaggerParseResult.getOpenAPI(), new ResolverCache(swaggerParseResult.getOpenAPI(), null, url)));
+      } else {
+        if (swaggerParseResult.getMessages().size() == 1 && swaggerParseResult.getMessages().get(0).matches("Unable to read location `?\\Q" + url + "\\E`?"))
+          future.fail(RouterFactoryException.createSpecNotExistsException(url));
+        else
+          future.fail(RouterFactoryException.createSpecInvalidException(StringUtils.join(swaggerParseResult.getMessages(), ", ")));
+      }
+    }).onComplete(promise);
+
+    return promise.future();
+  }
+}
