2b1ddbaddee391f2ac8604c0f4edb3ce3181035c
Pascal Krause <pascal.krause@sap.com>
2023-07-21 16:40:42 +0200

    feat(web-web-openapi): remove vertx-web-openapi
    Signed-off-by: Pascal Krause <pascal.krause@sap.com>

--- vertx-web-openapi/src/main/java/io/vertx/ext/web/openapi/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name = "vertx-openapi", groupPackage = "io.vertx", useFutures = true)
-package io.vertx.ext.web.openapi;
-
-import io.vertx.codegen.annotations.ModuleGen;
--- vertx-web-openapi/src/test/java/io/vertx/ext/web/openapi/it/services/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name="web-api-service", groupPackage = "io.vertx.ext.web.openapi.it.services", useFutures = true)
-package io.vertx.ext.web.openapi.it.services;
-
-import io.vertx.codegen.annotations.ModuleGen;
