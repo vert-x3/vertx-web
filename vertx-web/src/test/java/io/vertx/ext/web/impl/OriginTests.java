88820492347fed8c5f1d0853b1d7dcaf133d9650
Thomas Segismont <tsegismont@gmail.com>
2024-03-20 16:17:23 +0100

    CORS: support Chrome extensions (#2581)
    * CORS: support Chrome extensions
    
    See #2556
    
    This commit makes it possible to use star origin in CORS handler setup with a Chrome extension client app.
    Chrome Extensions use an origin of the follow form: chrome-extension://<extension-id>
    An extension id is 32 chars long and contains chars from 'a' to 'p' (0->9 + A->F translated to a->p).
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>
    
    * Review updates
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>
    
    ---------
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ vertx-web/src/test/java/io/vertx/ext/web/impl/OriginTests.java
@@ -0,0 +1,78 @@
+package io.vertx.ext.web.impl;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+@RunWith(Enclosed.class)
+public class OriginTests {
+
+  @RunWith(Parameterized.class)
+  public static class ValidOriginTest {
+
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> data() {
+      return Arrays.asList(new Object[][]{
+        {"null"},
+        {"http://www.vertx.io"},
+        {"http://[::1]"},
+        {"http://my-site.com"},
+        {"http://my-site.123.com"},
+        {"chrome-extension://gmbgaklkmjakoegficnlkhebmhkjfich"}
+      });
+    }
+
+    private final String origin;
+
+    public ValidOriginTest(String origin) {
+      this.origin = origin;
+    }
+
+    @Test
+    public void testValidOrigin() {
+      Origin.parse(origin); // does not fail
+      assertTrue(Origin.isValid(origin));
+    }
+  }
+
+
+  @RunWith(Parameterized.class)
+  public static class InvalidOriginTest {
+
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> data() {
+      return Arrays.asList(new Object[][]{
+        {"httpss://www.google.com"},
+        {"http://1[::1]:8080"},
+        {"http://my_site.com"},
+        {"http://1[::1]:8080"},
+        {"chrome-extension://gmbg0klkmjakoegficnlkhebmhkjfich"},
+        {"chrome-extension://gmbgaklkmjako"},
+      });
+    }
+
+    private final String origin;
+
+    public InvalidOriginTest(String origin) {
+      this.origin = origin;
+    }
+
+    @Test
+    public void testInvalidOrigin() {
+      try {
+        Origin.parse(origin);
+        fail("Should fail as it is a invalid origin: " + origin);
+      } catch (RuntimeException e) {
+        // OK
+      }
+
+      assertFalse(Origin.isValid(origin));
+    }
+  }
+}
