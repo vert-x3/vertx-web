4e6aa0f62912680cafda870c9f31a5421f558d89
vinceh121 <contact@vinceh121.me>
2024-04-09 21:26:02 +0200

    test: cache control max age with valid and invalid headers

---
+++ vertx-web-client/src/test/java/io/vertx/ext/web/client/impl/cache/CacheControlTest.java
@@ -0,0 +1,39 @@
+package io.vertx.ext.web.client.impl.cache;
+
+import java.time.Duration;
+
+import org.junit.Test;
+
+import io.vertx.core.MultiMap;
+import io.vertx.ext.web.client.WebClientTestBase;
+
+public class CacheControlTest extends WebClientTestBase {
+  @Test
+  public void testMaxAge() {
+    MultiMap headers = MultiMap.caseInsensitiveMultiMap();
+    headers.add("Date", "Tue, 09 Apr 2024 19:10:51 GMT");
+    headers.add("Expires", "Thu, 09 May 2024 19:10:51 GMT");
+
+    CacheControl cc = CacheControl.parse(headers);
+    assertEquals(Duration.ofDays(30).getSeconds(), cc.getMaxAge());
+  }
+
+  @Test
+  public void testInvalidHeaders() {
+    // invalid Date
+    MultiMap headers = MultiMap.caseInsensitiveMultiMap();
+    headers.add("Date", "this header doesn't make sense");
+    headers.add("Expires", "Thu, 09 May 2024 19:10:51 GMT");
+
+    CacheControl cc = CacheControl.parse(headers);
+    assertEquals(Long.MAX_VALUE, cc.getMaxAge());
+
+    // invalid Expires
+    headers = MultiMap.caseInsensitiveMultiMap();
+    headers.add("Date", "Tue, 09 Apr 2024 19:10:51 GMT");
+    headers.add("Expires", "this header doesn't make sense");
+
+    cc = CacheControl.parse(headers);
+    assertEquals(Long.MAX_VALUE, cc.getMaxAge());
+  }
+}
