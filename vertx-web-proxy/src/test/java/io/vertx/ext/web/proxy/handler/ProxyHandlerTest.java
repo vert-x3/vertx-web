543c6750a10507b653b5e74fb8034ec6896e7ab2
Thomas Segismont <tsegismont@gmail.com>
2025-03-26 21:45:57 +0100

    Improve ProtocolUpgradeHandler priority checking (#2726)
    See #2676
    
    ProtocolUpgradeHandler (GraphQLWS, Proxy) instances must not be installed after the BodyHandler.
    However, they are often used with authentication handlers (e.g. Basic auth handler), because authentication can be required before allowing the connection upgrade.
    The problem is that, currently, authn/authz handlers have higher priorities than body handlers (for good reasons, we may need to inspect the body to perform form login).
    So, if a user tries to install a ProtocolUpgradeHandler after an auth handler, an ISE is thrown.
    
    With this change, the order of priorities is not changed.
    Instead, there's a special check for ProtocolUpgradeHandler instances: the BodyHandler must not be found in the already installed handlers.
    
    Also, the warning about ProtocolUpgradeHandler being installed before another handler has been removed.
    Indeed, it is a fairly common case to hand over the request to another handler if the protocol upgrade has not been made (e.g. GraphQL handler installed after the GraphQLWS handler).
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-web-proxy/src/test/java/io/vertx/ext/web/proxy/handler/ProxyHandlerTest.java
+++ vertx-web-proxy/src/test/java/io/vertx/ext/web/proxy/handler/ProxyHandlerTest.java
@@ -1,7 +1,10 @@
 package io.vertx.ext.web.proxy.handler;
 
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.SimpleAuthenticationHandler;
 import io.vertx.ext.web.proxy.WebProxyTestBase;
 import io.vertx.httpproxy.HttpProxy;
 import org.junit.Test;
@@ -28,6 +31,21 @@ public class ProxyHandlerTest extends WebProxyTestBase {
     router.get("/path")
       .handler(BodyHandler.create())
       .handler(ProxyHandler.create(proxy));
+  }
+
+  @Test
+  public void shouldPassAuthHandlerOnSameRoute() throws Exception {
+    HttpProxy proxy = HttpProxy.reverseProxy(proxyClient);
+    proxy.origin(1234, "localhost");
+    router.get("/path")
+      .handler(SimpleAuthenticationHandler.create().authenticate(rc -> Future.succeededFuture(User.fromName("John"))))
+      .handler(ProxyHandler.create(proxy));
+    backendRouter.route(HttpMethod.GET, "/path").handler(rc -> {
+      rc.response().setStatusCode(200);
+      rc.response().setStatusMessage("statusMessage");
+      rc.response().end("data");
+    });
+    testRequest(HttpMethod.GET, "/path", 200, "statusMessage", "data");
   }
 
   @Test
