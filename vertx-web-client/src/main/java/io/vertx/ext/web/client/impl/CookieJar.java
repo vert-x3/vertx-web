d56f4532c00c0ba2fc9cfabcec933f01d10ac077
Julien Viet <julien@julienviet.com>
2026-06-17 22:31:24 +0200

    WebClient HTTP state management improvements.
    Motivation:
    
    The WebClient state management relies on the cookie store to perform state management.
    
    The implementation of state management is incomplete.
    
    Changes:
    
    Introduce a new CookieJar implementation that takes care of the HTTP state management.
    
    The cookie store now only takes care of being a cookie store with no management logic.

---
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/CookieJar.java
@@ -0,0 +1,228 @@
+package io.vertx.ext.web.client.impl;
+
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.vertx.core.http.impl.HttpUtils;
+import io.vertx.ext.web.client.spi.CookieStore;
+
+import java.util.*;
+
+public class CookieJar {
+
+  private final CookieStore store;
+
+  public CookieJar(CookieStore store) {
+    this.store = store;
+  }
+
+  public List<Cookie> cookies(boolean requestSecure, String requestDomain, String requestUri) {
+    long now = System.currentTimeMillis();
+    String path = extractPath(requestUri);
+    Iterable<Cookie> cookies = store.get((Boolean)null, requestDomain, path);
+    List<Cookie> list = new ArrayList<>();
+    List<Cookie> expired = null;
+    for (Cookie cookie : cookies) {
+      StoreCookie actual = (StoreCookie) cookie;
+      if (actual.hostOnly && !cookie.domain().equals(requestDomain)) {
+        // Check exact matching
+        continue;
+      }
+      if (cookie.isSecure() && !requestSecure) {
+        continue;
+      }
+      if (actual.expiryTime != null && actual.expiryTime <= now) {
+        if (expired == null) {
+          expired = new ArrayList<>();
+        }
+        expired.add(cookie);
+        continue;
+      }
+      list.add(cookie);
+    }
+    if (expired != null) {
+      for (Cookie cookie : expired) {
+        store.remove(cookie);
+      }
+    }
+    Collections.sort(list, new Comparator<Cookie>() {
+      @Override
+      public int compare(Cookie o1, Cookie o2) {
+        return compare((StoreCookie)o1, (StoreCookie)o2);
+      }
+      private int compare(StoreCookie o1, StoreCookie o2) {
+        int cmp = o1.path().compareTo(o2.path());
+        if (cmp == 0) {
+          cmp = Long.compare(o2.creationTime, o1.creationTime);
+        }
+        return -cmp;
+      }
+    });
+    return list;
+  }
+
+  public void setCookie(long creationTime, String domain, String requestUri, Cookie cookie) {
+    if (domain.isEmpty()) {
+      throw new IllegalArgumentException("Domain must not be empty");
+    }
+
+    // Compute the default-path
+    String path = extractPath(requestUri);
+    String defaultPath;
+    int lastSlashIdx;
+    if (path.isEmpty() || path.charAt(0) != '/' || path.length() == 1 || ((lastSlashIdx = path.lastIndexOf('/')) < 1)) {
+      defaultPath = "/";
+    } else {
+      defaultPath = path.substring(0, lastSlashIdx);
+    }
+    if (cookie.path() == null) {
+      cookie.setPath(defaultPath);
+    }
+    boolean hostOnly;
+    if (cookie.domain() == null) {
+      // Set the domain if missing, because we need to send cookies
+      // only to the domains we received them from.
+      cookie.setDomain(domain);
+      hostOnly = true;
+    } else if (cookie.domain().isEmpty()) {
+      throw new IllegalArgumentException("Cookie domain must not be empty");
+    } else if (domain.equals(cookie.domain()) || domain.endsWith("." + cookie.domain())) {
+      hostOnly = false;
+    } else {
+      return;
+    }
+
+    if (cookie.maxAge() == 0L) {
+      store.remove(cookie);
+    } else {
+
+      Long expiryTime;
+      if (cookie.maxAge() > 0L) {
+        expiryTime = creationTime + cookie.maxAge() * 1000;
+      } else {
+        expiryTime = null;
+      }
+
+      // Preserve creation time if we reinsert
+      StoreCookie prev = (StoreCookie) store.get(cookie.name(), cookie.domain(), cookie.path());
+      if (prev != null) {
+        creationTime = prev.creationTime;
+      }
+      store.put(new StoreCookie(cookie, hostOnly, creationTime, expiryTime));
+    }
+  }
+
+  static class StoreCookie implements Cookie {
+
+    private final Cookie actual;
+
+    // Metadata
+    private final boolean hostOnly;
+    private final long creationTime;
+    private final Long expiryTime;
+
+    public StoreCookie(Cookie actual, boolean hostOnly, long creationTime, Long expiryTime) {
+      this.actual = actual;
+      this.hostOnly = hostOnly;
+      this.creationTime = creationTime;
+      this.expiryTime = expiryTime;
+    }
+
+    @Override
+    public String name() {
+      return actual.name();
+    }
+
+    @Override
+    public String value() {
+      return actual.value();
+    }
+
+    @Override
+    public void setValue(String value) {
+      actual.setValue(value);
+    }
+
+    @Override
+    public boolean wrap() {
+      return actual.wrap();
+    }
+
+    @Override
+    public void setWrap(boolean wrap) {
+      actual.setWrap(wrap);
+    }
+
+    @Override
+    public String domain() {
+      return actual.domain();
+    }
+
+    @Override
+    public void setDomain(String domain) {
+      actual.setDomain(domain);
+    }
+
+    @Override
+    public String path() {
+      return actual.path();
+    }
+
+    @Override
+    public void setPath(String path) {
+      actual.setPath(path);
+    }
+
+    @Override
+    public long maxAge() {
+      return actual.maxAge();
+    }
+
+    @Override
+    public void setMaxAge(long maxAge) {
+      actual.setMaxAge(maxAge);
+    }
+
+    @Override
+    public boolean isSecure() {
+      return actual.isSecure();
+    }
+
+    @Override
+    public void setSecure(boolean secure) {
+      actual.setSecure(secure);
+    }
+
+    @Override
+    public boolean isHttpOnly() {
+      return actual.isHttpOnly();
+    }
+
+    @Override
+    public void setHttpOnly(boolean httpOnly) {
+      actual.setHttpOnly(httpOnly);
+    }
+
+    @Override
+    public int compareTo(Cookie o) {
+      return actual.compareTo(o);
+    }
+  }
+
+  private static String extractPath(String requestUri) {
+
+    requestUri = HttpUtils.removeDots(requestUri);
+
+    // Remove query params if present
+    int pos = requestUri.indexOf('?');
+    if (pos > -1) {
+      requestUri = requestUri.substring(0, pos);
+    }
+
+    // Remove fragment identifier if present
+    pos = requestUri.indexOf('#');
+    if (pos > -1) {
+      requestUri = requestUri.substring(0, pos);
+    }
+
+    return requestUri;
+  }
+}
--- vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
+++ vertx-web-client/src/main/java/io/vertx/ext/web/client/impl/SessionAwareInterceptor.java
@@ -1,6 +1,5 @@
 package io.vertx.ext.web.client.impl;
 
-import java.net.URI;
 import java.util.List;
 
 import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
@@ -8,9 +7,9 @@ import io.netty.handler.codec.http.cookie.ClientCookieEncoder;
 import io.netty.handler.codec.http.cookie.Cookie;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.RequestOptions;
-import io.vertx.ext.web.client.spi.CookieStore;
 
 /**
  * A stateless interceptor for session management that operates on the {@code HttpContext}
@@ -18,22 +17,37 @@ import io.vertx.ext.web.client.spi.CookieStore;
 public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
 
   private final WebClientSessionAware parentClient;
+  private final CookieJar cookieJar;
 
   public SessionAwareInterceptor(WebClientSessionAware clientSessionAware) {
     this.parentClient = clientSessionAware;
+    this.cookieJar = new CookieJar(clientSessionAware.cookieStore());
   }
 
   @Override
   public void handle(HttpContext<?> context) {
+    List<String> cookies;
+    String requestUri;
     switch(context.phase()) {
-      case CREATE_REQUEST:
-        createRequest(context);
+      case SEND_REQUEST:
+        sendRequest(context);
         break;
       case FOLLOW_REDIRECT:
-        processRedirectCookies(context);
+        cookies = context.clientResponse().cookies();
+        requestUri = context.clientResponse().request().getURI();
+        if (cookies != null) {
+          String host = context.clientResponse().request().getHost();
+          processResponseCookies(context, host, requestUri, cookies);
+        };
         break;
       case DISPATCH_RESPONSE:
-        processResponse(context);
+        cookies = context.response().cookies();
+        requestUri = context.request().uri();
+        if (cookies != null) {
+          RequestOptions request = context.requestOptions();
+          String host = context.request().virtualHost() != null ? context.request().virtualHost() : request.getHost();
+          processResponseCookies(context, host, requestUri, cookies);
+        }
         break;
       default:
         break;
@@ -42,128 +56,30 @@ public class SessionAwareInterceptor implements Handler<HttpContext<?>> {
     context.next();
   }
 
-  private void createRequest(HttpContext<?> context) {
+  private void sendRequest(HttpContext<?> context) {
+    HttpClientRequest request = context.clientRequest();
+    MultiMap headers = request.headers();
 
-    RequestOptions request = context.requestOptions();
-
-    RequestOptions requestOptions = context.requestOptions();
-    MultiMap headers = requestOptions.getHeaders();
-    if (headers == null) {
-      headers = HttpHeaders.headers();
-      requestOptions.setHeaders(headers);
-    }
+    // Handle client headers
     headers.addAll(parentClient.headers());
 
-    String domain = context.request().virtualHost();
-    if (domain == null) {
-      domain = requestOptions.getHost();
-    }
-
-    Iterable<Cookie> cookies = parentClient.cookieStore().get(request.isSsl(), domain, request.getURI());
+    // Set cookies
+    String domain = request.getHost();
+    boolean ssl = request.connection().isSsl();
+    Iterable<Cookie> cookies = cookieJar.cookies(ssl, domain, request.getURI());
     String encodedCookies = ClientCookieEncoder.STRICT.encode(cookies);
     if (encodedCookies != null) {
       headers.add(HttpHeaders.COOKIE, encodedCookies);
     }
   }
 
-  private void processRedirectCookies(HttpContext<?> context) {
-    this.processRedirectResponse(context);
-    this.prepareRedirectRequest(context);
-  }
-
-  private void processRedirectResponse(HttpContext<?> context) {
-    // Now the context contains the redirect request in clientRequest() and the original request in request()
-    List<String> cookieHeaders = context.clientResponse().cookies();
-    if (cookieHeaders == null) {
-      return;
-    }
-
-    RequestOptions originalRequest = context.requestOptions();
-    CookieStore cookieStore = parentClient.cookieStore();
-    String domain = URI.create(context.clientResponse().request().absoluteURI()).getHost();
-    if (domain.equals(originalRequest.getHost()) && context.request().virtualHost() != null) {
-      domain = context.request().virtualHost();
-    }
-    final String finalDomain = domain;
-    cookieHeaders.forEach(header -> {
-      Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
+  private void processResponseCookies(HttpContext<?> context, String host, String requestUri, List<String> cookieHeaders) {
+    long creationTime = System.currentTimeMillis();
+    for (String cookieHeader : cookieHeaders) {
+      Cookie cookie = ClientCookieDecoder.STRICT.decode(cookieHeader);
       if (cookie != null) {
-        if (cookie.domain() == null) {
-          // Set the domain if missing, because we need to send cookies
-          // only to the domains we received them from.
-          cookie.setDomain(finalDomain);
-        }
-        cookieStore.put(cookie);
-      }
-    });
-  }
-
-  private void prepareRedirectRequest(HttpContext<?> context) {
-    // Now the context contains the redirect request in clientRequest() and the original request in request()
-    RequestOptions redirectRequest = context.requestOptions();
-
-    RequestOptions originalRequest = context.requestOptions();
-    String redirectHost = redirectRequest.getHost();
-    String domain;
-    if (redirectHost.equals(originalRequest.getHost()) && context.request().virtualHost() != null) {
-      domain = context.request().virtualHost();
-    } else {
-      domain = redirectHost;
-    }
-
-    String path = parsePath(redirectRequest.getURI());
-    Iterable<Cookie> cookies = parentClient.cookieStore().get(originalRequest.isSsl(), domain, path);
-    String encodedCookies = ClientCookieEncoder.STRICT.encode(cookies);
-    if (encodedCookies != null) {
-      redirectRequest.putHeader(HttpHeaders.COOKIE, encodedCookies);
-    }
-  }
-
-  private static String parsePath(String uri) {
-    if (uri.length() == 0) {
-      return "";
-    }
-    int i;
-    if (uri.charAt(0) == '/') {
-      i = 0;
-    } else {
-      i = uri.indexOf("://");
-      if (i == -1) {
-        i = 0;
-      } else {
-        i = uri.indexOf('/', i + 3);
-        if (i == -1) {
-          // contains no /
-          return "/";
-        }
+        cookieJar.setCookie(creationTime, host, requestUri, cookie);
       }
     }
-
-    int queryStart = uri.indexOf('?', i);
-    if (queryStart == -1) {
-      queryStart = uri.length();
-    }
-    return uri.substring(i, queryStart);
-  }
-
-  private void processResponse(HttpContext<?> context) {
-    List<String> cookieHeaders = context.response().cookies();
-    if (cookieHeaders == null) {
-      return;
-    }
-
-    RequestOptions request = context.requestOptions();
-    CookieStore cookieStore = parentClient.cookieStore();
-    cookieHeaders.forEach(header -> {
-      Cookie cookie = ClientCookieDecoder.STRICT.decode(header);
-      if (cookie != null) {
-        if (cookie.domain() == null) {
-          // Set the domain if missing, because we need to send cookies
-          // only to the domains we received them from.
-          cookie.setDomain(context.request().virtualHost() != null ? context.request().virtualHost() : request.getHost());
-        }
-        cookieStore.put(cookie);
-      }
-    });
   }
 }
